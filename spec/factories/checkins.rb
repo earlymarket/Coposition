9bc9186dc2eb618d24152190a027b531bf8eef0a
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-08 15:55:00 +0100

    Created Factory + implemented request fixture

---
+++ spec/factories/checkins.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :checkin do
+    status "A"
+    lat { Faker::Address.latitude }
+    n_s { ["N", "S"].shuffle.first }
+    lng { Faker::Address.longitude }
+    e_w { ["E", "W"].shuffle.first }
+    gspeed { Faker::Number.between(0, 130) + (Faker::Number.between(0, 99) / 100) }
+    altitude { Faker::Number.between(0, 8000) + (Faker::Number.between(0, 99) / 100) }
+    course { Faker::Number.between(0, 359) + (Faker::Number.between(0, 99) / 100) }
+    time { Time.now.strftime("%H%M%S.%L") }
+    date { Time.now.strftime("%d%m%y") }
+    rotorspeed { Faker::Number.between(0, 600) + (Faker::Number.between(0, 99) / 100) }
+    enginespeed { Faker::Number.between(0, 3000) + (Faker::Number.between(0, 99) / 100) }
+  end
+
+end
---
+++ spec/fixtures/request_fixture.rb
@@ -0,0 +1,16 @@
+class RequestFixture
+
+  class << self
+
+    def w_gps
+      # Including GPS data
+      "A|2307.1256|N|12016.4438|E|0.03|13|165.48|" + no_gps
+    end
+
+    def no_gps
+      # Excluding GPS data
+      "064951.000|260406|490.01|1314.21"
+    end
+
+  end
+end
