1a991ad2d528b9f82a634179572d015c28c2b2eb
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-10 15:43:27 +0100

    Don't need code generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def connection_code
-    return super unless super.nil?
-    code = []
-    encrypted_password[0,6].each_byte do |byte|
-      code << (byte % 3) + 1
-    end
-    str = code.join
-    self.connection_code = str
-    save
-    "test"
-  end
+   
 end
