1fb4ee5d30f5479db1011be4df07bb3726da3bb9
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-11 16:19:25 +0100

    Using dynamic methods for new and create

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,20 +2,21 @@ class Checkin < ActiveRecord::Base
 
   belongs_to :device
 
+  [:new, :create].each do |prefix|
+    define_singleton_method("#{prefix}_from_string") do |string|
+      hash = to_hash(string)
+
+      device = Device.where(imei: hash[:imei]).first
+      device = Device.create(imei: hash[:imei]) unless device
+      new_checkin = send(prefix, hash)
+      device.checkins << new_checkin
+      new_checkin
+    end
+  end
+
   def self.string_order
     [:imei,:enginespeed,:rotorspeed,:date,:time,:course,:altitude,
       :gspeed,:e_w,:lng,:n_s,:lat,:status]
-  end
-
-  def self.create_from_string (string)
-    hash = to_hash(string)
-
-    device = Device.where(imei: hash[:imei]).first
-    device = Device.create(imei: hash[:imei]) unless device
-    new_checkin = create(hash)
-    # binding.pry
-    device.checkins << new_checkin
-    new_checkin
   end
 
   protected
