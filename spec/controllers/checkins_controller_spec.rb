9f233feec43b8662c00720447db6d972322c5f98
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-11 11:23:03 +0100

    Added controller spec

--- spec/controllers/checkins_controller_spec.rb
+++ spec/controllers/checkins_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe CheckinsController, type: :controller do
+RSpec.describe Redbox::CheckinsController, type: :controller do
+
+  describe "posting" do
+    it "should POST to the server with a normal string" do
+      post "/redbox/checkins", RequestFixture.w_gps
+      expect(last_response.ok?).to be true
+
+      # Don't send entire obj back due to GPRS limits
+      expect(last_response.body).to eq "ok"
+    end
+  end
 
 end
