b161b8eb7120a19c140aafcb1c5d81a2eef508d4
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-14 16:13:07 +0100

    Added helper methods for Devise

---
+++ spec/controllers/connections_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Redbox::ConnectionsController, type: :controller do
+
+  login_user
+
+  it "should have a current_user" do
+    subject.current_user.should_not be_nil
+  end
+
+  describe "posting" do
+
+    before do
+      @checkin = Checkin.create_from_string(RequestFixture.w_gps, add_device: true)
+    end
+
+    it "should POST to with an IMEI" do
+      # For some reason, subject.current user was returning some weird results. Using last User instead
+      @user = User.last
+      post :create, imei: @checkin.imei
+
+      expect(response.code).to eq "302"
+      expect(@user.devices.count).to be 1
+      expect(@user.devices.last).to eq @checkin.device
+    end
+
+  end
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,10 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'devise'
+
+
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -21,6 +25,7 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 Dir[Rails.root.join('spec/fixtures/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -49,4 +54,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
+
 end
