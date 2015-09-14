46c6eb30fa42ae17cb57beb8101efc8129271675
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-14 10:56:04 +0100

    Loading in fixture

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -31,5 +32,7 @@ module Coposition
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    require "./lib/request_fixture"
   end
 end
