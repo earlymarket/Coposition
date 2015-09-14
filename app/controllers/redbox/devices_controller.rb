9ea0764961b002a2442f3e4655f45589c7a747fc
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-14 17:41:00 +0100

    Renames + Instantiating new device under new

--- app/controllers/redbox/devices_controller.rb
+++ app/controllers/redbox/devices_controller.rb
@@ -11,29 +11,31 @@ class Redbox::DevicesController < ApplicationController
   end
 
   def new
+    @device = Device.new
   end
 
   def create
-    device = Device.find_by imei: allowed_params
+    device = Device.find_by imei: allowed_params[:imei]
     if device
       # Providing that there isn't anyone currently assigned
       if device.user.nil?
         device.user = current_user
+        device.name = allowed_params[:name]
         device.save
         flash[:notice] = "This device has been bound to your account!"
-        redirect_to root_path
+        redirect_to redbox_devices_path
       else
         flash[:alert] = "This device has already been assigned an account!"
-        redirect_to new_redbox_connection_path
+        redirect_to new_redbox_device_path
       end
     else
       flash[:alert] = "Not found"
-      redirect_to new_redbox_connection_path
+      redirect_to new_redbox_device_path
     end
   end
 
   private
   def allowed_params
-    params.require(:imei)
+    params.require(:device).permit(:imei,:name)
   end
 end
