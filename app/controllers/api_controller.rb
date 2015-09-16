4fd9f9fd5df38aa44e27dd36fcb97426f33d9ed3
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-16 12:22:01 +0100

    Added member get and response to API doc

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,7 @@ class ApiController < ApplicationController
   def index
     @w_gps = RequestFixture.w_gps
     @mapping = Checkin.string_order
+    @w_gps_response = JSON.pretty_generate(Checkin.first.as_json)
   end
 
 end
