e7e17721b49e45f9d9fd85d94d836672afd1c2f5
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-23 10:21:31 +0100

    Remigrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923090102) do
-
+ActiveRecord::Schema.define(version: 20150922093412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
