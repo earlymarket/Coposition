5f76f8c0d9dbdfd1a87c83bf7335a294d537a3f6
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-25 17:26:19 +0100

    Removing test sleep

--- app/controllers/users/dashboards_controller.rb
+++ app/controllers/users/dashboards_controller.rb
@@ -3,7 +3,6 @@ class Users::DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    sleep 1
     flash[:notification] = current_user.notifications
   end
 
