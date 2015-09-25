99314b637600bd63bfe387fc1328c014522a0d65
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-25 17:15:57 +0100

    Fancy page transition

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,7 +2,13 @@ $(document).on('page:change', function(){
   setup();
 });
 
+$(document).on('page:fetch', function() {
+  utility.exitPage();
+});
+
+
 function setup() {
+  utility.enterPage();
   addEventListeners();
 };
 
@@ -14,6 +20,6 @@ function addEventListeners() {
 
 function addClickListeners() {
   $(".close").click(function(e){
-    $(e.currentTarget).parent().addClass("flipOutX");
+    $(e.currentTarget).parent().addClass("flipOutX").slideUp(1000);
   });
 };
--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -1,3 +1,10 @@
 var utility = {
-  
+  enterPage: function(){
+    console.log("enter");
+    $("#container").removeClass("slideOutLeft").addClass("slideInRight")
+  },
+  exitPage: function(){
+    console.log("exit");
+    $("#container").removeClass("slideInRight").addClass("slideOutLeft")
+  }
 }
