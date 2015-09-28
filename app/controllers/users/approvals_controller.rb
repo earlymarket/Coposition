dc6ab5ad19f4c296f875c8b1eb7763c694e4c81f
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-28 11:39:58 +0100

    Added approvals/approve logic

--- app/controllers/users/approvals_controller.rb
+++ app/controllers/users/approvals_controller.rb
@@ -3,7 +3,17 @@ class Users::ApprovalsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @approvals = current_user.pending_approvals
+    @pending_approvals = current_user.pending_approvals
+  end
+
+  def approve
+    @approval = Approval.where(id: params[:id], 
+      user: current_user).first
+    @approval.approve!
+  end
+
+  def deny
+    binding.pry
   end
 
 end
