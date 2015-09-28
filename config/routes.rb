77db1dd0eac744f886ac4b07d5412e4988de66f5
Daniel Steele <danielsteele@hotmail.co.uk>
2015-09-28 09:50:15 +0100

    Added routes for accepting/denying approvals

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,12 @@ Rails.application.routes.draw do
 
   namespace :users do
     resource :dashboard, only: [:show]
-    resources :approvals, only: [:index]
+    resources :approvals, only: [:index] do
+      member do
+        get "approve"
+        get "reject"
+      end
+    end
   end
 
   namespace :developers do
