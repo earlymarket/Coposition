7ea3da4a42390d9161aadbb4a76feda1e64ee930
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-05 14:36:38 +0100

    Added test to ensure appending to device if UUID already exists

--- spec/controllers/api/v1/checkins_controller_spec.rb
+++ spec/controllers/api/v1/checkins_controller_spec.rb
@@ -14,12 +14,38 @@ RSpec.describe Api::V1::CheckinsController, type: :controller do
 						lng: Faker::Address.longitude
 					}
 				}
+
 			post :create, @demo
 			res = response_to_hash
 			
 			expect(res[:uuid]).to eq @demo[:checkin][:uuid]
 			expect(Checkin.count).to be (checkin_count + 1)
 			expect(Device.count).to be (device_count + 1)
+		end
+
+		it "should POST a checkin with a pre-existing device" do
+			uuid = Faker::Number.number(12)
+
+			checkin_count = Checkin.count
+
+			device = FactoryGirl::create(:device)
+			device.uuid = uuid
+			device.save!
+
+			@demo = {
+				checkin: {
+						uuid: uuid,
+						lat: Faker::Address.latitude,
+						lng: Faker::Address.longitude
+					}
+				}
+
+			post :create, @demo
+			res = response_to_hash
+			
+			expect(res[:uuid]).to eq @demo[:checkin][:uuid]
+			expect(Checkin.count).to be (checkin_count + 1)
+			expect(Device.find_by(uuid: uuid)).to_not be nil
 		end
 
 	end
