a3962416119a245cbb3b16cfef4ab37e2199136d
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-06 10:58:07 +0100

    Added users spec and devices factory

---
+++ spec/controllers/api/v1/users/devices_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Users::DevicesController, type: :controller do
+  include ControllerMacros
+
+
+  describe "GET" do
+    before do
+    	@user = FactoryGirl::create :user
+      @device = FactoryGirl::create :device
+    	@developer = FactoryGirl::create :developer
+    	@user.devices << @device
+    	@user.save!
+      @developer.request_approval_from(@user)
+      @user.approve_developer(@developer)
+      request.headers["X-Api-Key"] = @developer.api_key
+  	end
+		it "should GET a list of devices of a specific user" do
+			get :index, user_id: @user.id, format: :json
+      res = response_to_hash
+      expect(res.first["id"]).to be @device.id
+    end
+
+    it "should GET information on a specific device for a specific user" do
+      get :show, user_id: @user.username, id: @device.id, format: :json
+      res = response_to_hash
+      expect(res.first["id"]).to be @device.id
+		end
+	end
+
+end
---
+++ spec/factories/devices.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :device do
+    uuid { Faker::Number.number(12) }
+    name { Faker::Commerce.product_name }
+  end
+
+end
