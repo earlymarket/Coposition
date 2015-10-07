281c93714754057e9de3a3ca500246585eef34f2
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-07 18:01:50 +0100

    Added relationship spec

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -8,11 +8,18 @@ RSpec.describe Device, type: :model do
 
   describe "realtionships" do
 
-    it "should have some checkins" do
+    it "should have some redbox checkins" do
       redbox_checkins = [FactoryGirl::create(:redbox_checkin)]
       redbox_checkins << FactoryGirl::create(:redbox_checkin)
       @device.redbox_checkins << redbox_checkins
       expect(@device.redbox_checkins).to match_array(redbox_checkins)
+    end
+
+    it "should have some checkins" do
+      checkins = [FactoryGirl::create(:checkin)]
+      checkins << FactoryGirl::create(:checkin)
+      @device.checkins << checkins
+      expect(@device.checkins).to match_array(checkins)
     end
 
   end
