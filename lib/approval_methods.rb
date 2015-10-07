79a2bd61d5f1d6f4b4872ef4a73f04dee53b534c
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-07 09:33:23 +0100

    Added getter for approval status

--- lib/approval_methods.rb
+++ lib/approval_methods.rb
@@ -28,4 +28,9 @@ module ApprovalMethods
     app && app.approved?
   end
 
+  def approval_status_for(model)
+    app = approvals.where({ model.class.to_s.downcase.to_sym => model }).first
+    app.approved? if app
+  end
+
 end
