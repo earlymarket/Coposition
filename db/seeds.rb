ed0c22dc8987ab657a4ce1d3265d8ed3e4149d99
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-07 18:01:25 +0100

    Added seeds for deom

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Setup user/dev for example site
+user = FactoryGirl::build :user
+user.username = "coporulez"
+user.save!
+
+device = FactoryGirl::create :device
+user.devices << device
+user.save!
+
+device.checkins << ( FactoryGirl::create :checkin )
+device.save!
+
+developer = FactoryGirl::build :developer
+developer.save!
+developer.company_name = "Demo developer account"
+developer.api_key = "HAQXbCZ12JazSyyERk6CZAtt"
+developer.save!
