eeb476acc61f3cf093faec4f2ad610dd63afa5e2
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-07 09:32:16 +0100

    Don't want to check approved here. (also therefore @user not in scope

--- app/controllers/api/v1/users/approvals_controller.rb
+++ app/controllers/api/v1/users/approvals_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Users::ApprovalsController < Api::ApiController
   respond_to :json
 
-  before_action :authenticate, :check_user_approved_developer
+  before_action :authenticate
 
   def create
   	# For some reason respond_with doesn't work here
   	# TODO: research why
-    render json: @dev.request_approval_from(@user)
+    render json: @dev.request_approval_from(User.find(params[:user_id]))
   end
 
 end
