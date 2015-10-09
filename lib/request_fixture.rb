5816b05078f61e5aed97901fbaee9ac88e3670d4
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-09 17:54:33 +0100

    IMEI ==> UUID. Also using normal lat/lngs for now

--- lib/request_fixture.rb
+++ lib/request_fixture.rb
@@ -1,17 +1,18 @@
 class RequestFixture
 
-  def initialize(imei="356938035643809")
-    @imei = imei
+  def initialize(uuid="356938035643809")
+    @uuid = uuid
   end
 
   def w_gps
     # Including GPS data
-    no_gps + "165.48|13|0.03|E|12016.4438|N|2307.1256|A"
+    # Redbox version (temp disabled) no_gps + "165.48|13|0.03|E|12016.4438|N|2307.1256|A"
+    no_gps + "165.48|13|0.03|E|-0.513069|N|51.588330|A"
   end
 
   def no_gps
     # Excluding GPS data
-    @imei + "|1314.21|490.01|260406|064951.000|"
+    @uuid + "|1314.21|490.01|260406|064951.000|"
   end
 
 end
