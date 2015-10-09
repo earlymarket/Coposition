ea2e9d311acfa86a3a6510b95363f04c342acc7f
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-09 17:04:01 +0100

    Added explicit geocoding method - other one was too implicit.

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -3,12 +3,12 @@ class Checkin < ActiveRecord::Base
   belongs_to :device
 
   reverse_geocoded_by :lat, :lng do |obj,results|
-	  if geo = results.first
-	    obj.city    = geo.city
-	    obj.postal_code = geo.postal_code
-	    obj.country = geo.country_code
-	  end
-	end
+    if geo = results.first
+      obj.city    = geo.city
+      obj.postal_code = geo.postal_code
+      obj.country = geo.country_code
+    end
+  end
 
 
   after_create do
@@ -17,23 +17,32 @@ class Checkin < ActiveRecord::Base
     device.checkins << self
   end
 
-  def self.find_range(from, size)
-    order(:id).find(range_array(from, size))
+  class << self
+
+    def find_range(from, size)
+      order(:id).find(range_array(from, size))
+    end
+
+    def non_geocoded_keys
+      column_names - geocoded_keys
+    end
+
+    def geocoded_keys
+      %w{address city country postal_code}
+    end
+
   end
 
-  def city
-  	reverse_geocode unless address
-  	super
+  def non_geocoded_data
+    self.as_json.except(Checkin.geocoded_keys)
   end
 
-  def country
-  	reverse_geocode unless address
-  	super
+  def reverse_geocode!
+    reverse_geocode unless reverse_geocoded?
   end
 
-  def postal_code
-  	reverse_geocode unless address
-  	super
+  def reverse_geocoded?
+    !address.nil?
   end
 
   protected
