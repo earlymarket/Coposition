362a7d8b77af819d1f191d25dec6e67b812468dd
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-13 17:10:08 +0100

    Added return for null

--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -1,6 +1,7 @@
 var utility = {
   urlParam: function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (!results) return null; 
     return results[1] || 0;
   },
   animations: {
