cd7406de1978167128434cd9f3b74ec12617f636
Daniel Steele <danielsteele@hotmail.co.uk>
2015-10-15 12:06:08 +0100

    Not using scope

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,13 +2,4 @@ class Device < ActiveRecord::Base
   belongs_to :user
   has_many :checkins
   has_many :redbox_checkins
-
-  # scope :last_checkin, -> { checkins.last }
-
-  def last_checkin
-    checkins.last
-  end
 end
-
-
-FIX DIS
