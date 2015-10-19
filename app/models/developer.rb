df3687abd48e2a94ce5a969a43c063198a4134df
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-19 10:18:05 +0100

    Tests passing with refactor into models

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,5 @@
 class Developer < ActiveRecord::Base
-  include ApprovalMethods
+  # include ApprovalMethods
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -9,6 +9,8 @@ class Developer < ActiveRecord::Base
 
   has_many :approvals
   has_many :users, through: :approvals
+
+
 
   before_create do |dev|
     dev.api_key = generate_api_key
@@ -20,6 +22,25 @@ class Developer < ActiveRecord::Base
       break token unless Developer.exists?(api_key: token)
     end
   end
+
+  def pending_approvals
+    approvals.where(pending: true)
+  end
+
+  def approved_users
+    approvals.where(approved: true)
+  end
+  
+  def approval_status_for(model)
+    app = approvals.where({ model.class.to_s.downcase.to_sym => model }).first
+    app.approved? if app
+  end
+
+  def request_approval_from(user)
+    approvals << Approval.create(developer: self, user: user)
+  end
+  
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   extend FriendlyId
   friendly_id :username, use: [:slugged, :finders]
 
-  include ApprovalMethods
+  # include ApprovalMethods
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable #:omniauthable
@@ -14,6 +14,36 @@ class User < ActiveRecord::Base
   has_many :devices
   has_many :approvals
   has_many :developers, through: :approvals
+
+
+
+  def pending_approvals
+    approvals.where(pending: true)
+  end
+
+  def approved_developers
+    approvals.where(approved: true)
+  end
+
+  def approval_status_for(model)
+    app = approvals.where({ model.class.to_s.downcase.to_sym => model }).first
+    app.approved? if app
+  end
+
+  def approve_developer(dev)
+    app = approvals.where(approved: false, developer: dev).first
+    unless app
+      return false
+    end
+    app.approved = true
+    app.pending = false
+    app.save
+  end
+
+  def approved_developer?(dev)
+    app = approvals.where(developer: dev).first
+    app && app.approved?
+  end
 
   def notifications
     x = []
