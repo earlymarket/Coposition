4106437550606e16a4759eba5b47d05be4eb3a79
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-22 16:11:30 +0100

    Added model tests

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Device, type: :model do
 
   before do
+    @developer = FactoryGirl::create :developer
     @device = Device.create(uuid: 1234)
   end
 
@@ -23,11 +24,31 @@ RSpec.describe Device, type: :model do
     end
 
     it "should have some approved devleopers" do
-      @developer = FactoryGirl::create :developer
       @device.developers << @developer
       expect( @device.developers.first ).to eq @developer
     end
 
+  end
+
+  it "should get the privilege level for a developer" do
+    @device.developers << @developer
+    expect(@device.privilege_for @developer).to eq "complete"
+  end
+
+  it "should change privilege levels for a developer" do
+    @device.developers << @developer
+    expect(@device.privilege_for @developer).to eq "complete"
+    @device.change_privilege_for(@developer, 2)
+
+    expect(@device.privilege_for @developer).to eq "disallowed"
+
+    @device.change_privilege_for(@developer, "complete")
+
+    expect(@device.privilege_for @developer).to eq "complete"
+
+    @device.change_privilege_for(@developer, @device.reverse_privilege_for(@developer))
+  
+    expect(@device.privilege_for @developer).to eq "disallowed"
   end
 
 end
