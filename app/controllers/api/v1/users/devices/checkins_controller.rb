5dae404417870298e1788c7455c7a5f7c9b87f1d
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-23 10:25:44 +0100

    Calling check privilege

--- app/controllers/api/v1/users/devices/checkins_controller.rb
+++ app/controllers/api/v1/users/devices/checkins_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Users::Devices::CheckinsController < Api::ApiController
   respond_to :json
 
-  before_action :authenticate, :check_user_approved_developer, :find_device
+  before_action :authenticate, :check_user_approved_developer, :find_device, :check_privilege
 
   def last
 		checkin = @device.checkins.last
