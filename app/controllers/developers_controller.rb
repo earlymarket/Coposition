aa51afc960caee953b2769b31768914d831b6c85
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-27 17:11:03 +0000

    Added update action

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -6,4 +6,13 @@ class DevelopersController < ApplicationController
     @developer = current_developer
   end
 
+  def update
+    current_developer.update(allowed_params)
+  end
+
+  def allowed_params
+    params.require(:developer).permit([:company_name, :logo])
+    redirect_to developer_console_path
+  end
+
 end
