3fd0317c7fd4494ae1474d4707a2235a4c52fa1f
Dan Steele <danielsteele@hotmail.co.uk>
2015-10-28 11:01:01 +0000

    Added public/system to .gitignore

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -8,11 +8,11 @@ class DevelopersController < ApplicationController
 
   def update
     current_developer.update(allowed_params)
+    redirect_to developers_console_path
   end
 
   def allowed_params
     params.require(:developer).permit([:company_name, :logo])
-    redirect_to developer_console_path
   end
 
 end
