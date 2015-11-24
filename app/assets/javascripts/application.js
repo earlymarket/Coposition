609579ceccf3f000cd38c6e3aafa37a101aad31f
Dan Steele <danielsteele@hotmail.co.uk>
2015-11-24 12:57:10 +0000

    Precompiling assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+#// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require lettering
+//= require toastr.min
+//= require utility
 //= require_tree .
