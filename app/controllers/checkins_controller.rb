1a4b022e5168f728590ce1cd807b0980d54339cc
Dan Steele <danielsteele@hotmail.co.uk>
2015-11-25 13:26:01 +0000

    Making Checkin specific

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -2,25 +2,22 @@ class CheckinsController < ApplicationController
 
   protect_from_forgery except: :create
 
-  # Using @@model so Redbox::CheckinsController has access to same method
-  @@model = Checkin
-
   def index
-    @checkin_count = @@model.count
+    @checkin_count = Checkin.count
   end
 
   def show
-    checkin = @@model.find(params[:id])
+    checkin = Checkin.find(params[:id])
     unless params[:range] 
       render json: checkin.to_json
     else
       # /2?range=3 returns IDs 2,3,4
-      render json: @@model.find_range(checkin.id, params[:range].to_i)
+      render json: Checkin.find_range(checkin.id, params[:range].to_i)
     end
   end
 
   def destroy
-    @@model.where(device: params[:id]).destroy_all if user_owns_device?
+    Checkin.where(device: params[:id]).destroy_all if user_owns_device?
     flash[:notice] = "History deleted."
     redirect_to user_device_path(current_user.id, params[:id])
   end
