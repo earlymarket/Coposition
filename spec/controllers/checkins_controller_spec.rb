41574df6728911bf792c35de25050d39dee9ff5f
Dan Steele <danielsteele@hotmail.co.uk>
2015-11-25 13:26:10 +0000

    Commenting out

--- spec/controllers/checkins_controller_spec.rb
+++ spec/controllers/checkins_controller_spec.rb
@@ -1,34 +1,35 @@
-require 'rails_helper'
+#       # REDBOXTODO
+# require 'rails_helper'
 
-RSpec.describe Redbox::CheckinsController, type: :controller do
+# RSpec.describe Redbox::CheckinsController, type: :controller do
 
-  describe "posting" do
-    it "should POST to the server with a normal string" do
-      post :create, data: RequestFixture.new.w_gps
-      expect(response.ok?).to be true
+#   describe "posting" do
+#     it "should POST to the server with a normal string" do
+#       post :create, data: RequestFixture.new.w_gps
+#       expect(response.ok?).to be true
 
-      # Don't send entire obj back due to GPRS limits
-      expect(response.body).to eq "ok"
-    end
+#       # Don't send entire obj back due to GPRS limits
+#       expect(response.body).to eq "ok"
+#     end
 
-    it "should POST to the server without string" do
-      post :create, data: RequestFixture.new.no_gps
-      expect(response.ok?).to be true
+#     it "should POST to the server without string" do
+#       post :create, data: RequestFixture.new.no_gps
+#       expect(response.ok?).to be true
 
-      # Don't send entire obj back due to GPRS limits
-      expect(response.body).to eq "ok"
-    end
+#       # Don't send entire obj back due to GPRS limits
+#       expect(response.body).to eq "ok"
+#     end
 
-    it "should GET a range" do
-      @chk1 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
-      @chk2 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
-      @chk3 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
-      @chk4 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
-      post :show, {id: @chk1.id, range: 3}
-      expect(response.ok?).to be true
+#     it "should GET a range" do
+#       # @chk1 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
+#       # @chk2 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
+#       # @chk3 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
+#       # @chk4 = RedboxCheckin.create_from_string(RequestFixture.new.w_gps)
+#       # post :show, {id: @chk1.id, range: 3}
+#       # expect(response.ok?).to be true
 
-      expect(response.body).to eq [@chk1, @chk2, @chk3].to_json
-    end
-  end
+#       # expect(response.body).to eq [@chk1, @chk2, @chk3].to_json
+#     end
+#   end
 
-end
+# end
