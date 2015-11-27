c8aee37f0fe20e30c4d363352d33a614349c3b80
Dan Steele <danielsteele@hotmail.co.uk>
2015-11-27 14:00:37 +0000

    Added delayed? field to device

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123140935) do
+ActiveRecord::Schema.define(version: 20151127104219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20151123140935) do
     t.integer "user_id"
     t.string  "name"
     t.boolean "fogged",  default: false
+    t.integer "delayed"
   end
 
   add_index "devices", ["uuid"], name: "index_devices_on_uuid", using: :btree
--- spec/factories/devices.rb
+++ spec/factories/devices.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :device do
     uuid { Faker::Number.number(12) }
     name { Faker::Commerce.product_name }
+    delayed { [true, false].sample  }
   end
 
 end
