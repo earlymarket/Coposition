dd31a35a785366e13eb92f3473f7334569c7dcc1
Gary Siu <Gary@untypeable.com>
2015-12-10 09:59:50 +0000

    Refactored approvals_controller

--- spec/controllers/api/v1/users/approvals_controller_spec.rb
+++ spec/controllers/api/v1/users/approvals_controller_spec.rb
@@ -5,31 +5,32 @@ RSpec.describe Api::V1::Users::ApprovalsController, type: :controller do
 
   describe "a developer" do
 
+    let(:user){FactoryGirl::create :user}
+    let(:developer){FactoryGirl::create :developer}
+
     before do
-      @user = FactoryGirl::create :user
-      @developer = FactoryGirl::create :developer
-      request.headers["X-Api-Key"] = @developer.api_key
+      request.headers["X-Api-Key"] = developer.api_key
     end
 
     it "should be able to submit an approval request" do
-      post :create, user_id: @user.username, format: :json
+      post :create, user_id: user.username, format: :json
 
       expect(response.status).to be 200
-      expect(@user.pending_approvals.count).to be 1
+      expect(user.pending_approvals.count).to be 1
     end
 
     it "should be told if the approval is still pending" do
       # No approval
-      get :status, user_id: @user.username, format: :json
+      get :status, user_id: user.username, format: :json
       expect(res_hash[:approval_status]).to be nil
 
-      @developer.request_approval_from @user
-      get :status, user_id: @user.username, format: :json
+      developer.request_approval_from user
+      get :status, user_id: user.username, format: :json
       expect(res_hash[:approval_status]).to be false
 
 
-      @user.approve_developer @developer
-      get :status, user_id: @user.username, format: :json
+      user.approve_developer developer
+      get :status, user_id: user.username, format: :json
       expect(res_hash[:approval_status]).to be true
     end
 
