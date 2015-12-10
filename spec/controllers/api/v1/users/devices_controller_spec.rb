4dbe9d252d726cf191a34f54c61979a5c79c77c5
Gary Siu <Gary@untypeable.com>
2015-12-10 10:12:59 +0000

    Refactored developer and devices in devices_controller

--- spec/controllers/api/v1/users/devices_controller_spec.rb
+++ spec/controllers/api/v1/users/devices_controller_spec.rb
@@ -5,61 +5,63 @@ RSpec.describe Api::V1::Users::DevicesController, type: :controller do
 
 
   describe "GET" do
+
+    let(:device){FactoryGirl::create :device}
+    let(:developer){FactoryGirl::create :developer}
+
     before do
-    	@user = FactoryGirl::create :user
-      @device = FactoryGirl::create :device
-    	@developer = FactoryGirl::create :developer
-    	@user.devices << @device
-    	@user.save!
+      @user = FactoryGirl::create :user
+      @user.devices << device
+      @user.save!
       @checkin = FactoryGirl::build :checkin
-      @checkin.uuid = @device.uuid
+      @checkin.uuid = device.uuid
       @checkin.save
-      @developer.request_approval_from(@user)
-      @user.approve_developer(@developer)
-      request.headers["X-Api-Key"] = @developer.api_key
-  	end
+      developer.request_approval_from(@user)
+      @user.approve_developer(developer)
+      request.headers["X-Api-Key"] = developer.api_key
+    end
 
 
 
     it "should GET a list of devices of a specific user" do
       get :index, user_id: @user.username, format: :json
-      expect(res_hash.first["id"]).to be @device.id
+      expect(res_hash.first["id"]).to be device.id
     end
 
     it "should record the request" do
-      expect(@developer.requests.count).to be 0
+      expect(developer.requests.count).to be 0
       get :index, user_id: @user.username, format: :json
-      expect(@developer.requests.count).to be 1
+      expect(developer.requests.count).to be 1
     end
 
     it "should GET information on a specific device for a specific user" do
-      get :show, user_id: @user.username, id: @device.id, format: :json
-      expect(res_hash.first["id"]).to be @device.id
-		end
+      get :show, user_id: @user.username, id: device.id, format: :json
+      expect(res_hash.first["id"]).to be device.id
+    end
 
     it "should let you know the last checkin for a list of devices if there is one" do
-      get :index, user_id: @user.username, id: @device.id, format: :json
+      get :index, user_id: @user.username, id: device.id, format: :json
       expect(res_hash.first["last_checkin"]["id"]).to eq @checkin.id
     end
 
     it "should let you know the last checkin for device if there is one" do
-      get :show, user_id: @user.username, id: @device.id, format: :json
+      get :show, user_id: @user.username, id: device.id, format: :json
       expect(res_hash.first["last_checkin"]["id"]).to eq @checkin.id
     end
 
     it "should not only return devices for which the developer has permission" do
-      @device.change_privilege_for @developer, "disallowed"
+      device.change_privilege_for developer, "disallowed"
       get :index, user_id: @user.username, format: :json
       expect(response.body).to eq "[]"
       expect(response.status).to be 200
     end
 
     it "should not allow a developer to see a device for which it disallowed" do
-      @device.change_privilege_for @developer, "disallowed"
-      get :show, user_id: @user.username, id: @device.id, format: :json
+      device.change_privilege_for developer, "disallowed"
+      get :show, user_id: @user.username, id: device.id, format: :json
       expect(response.body).to eq ""
       expect(response.status).to be 401
     end
-	end
+  end
 
 end
