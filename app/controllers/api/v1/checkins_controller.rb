59ac6ed45bb2d58dc5662d1b1ad99aae111ec661
t-harps <t.w.harps@gmail.com>
2015-12-15 13:05:03 +0000

    Converted tabs to spaces

--- app/controllers/api/v1/checkins_controller.rb
+++ app/controllers/api/v1/checkins_controller.rb
@@ -2,17 +2,17 @@ class Api::V1::CheckinsController < Api::ApiController
   respond_to :json
 
   def create
-  	return unless valid_request?
+    return unless valid_request?
     render json: Checkin.create(allowed_params)
   end
 
   private
 
-	  def allowed_params
-	    params.require(:checkin).permit(:uuid, :lat, :lng)
-	  end
+    def allowed_params
+      params.require(:checkin).permit(:uuid, :lat, :lng)
+    end
 
-  	def valid_request?
+    def valid_request?
       if allowed_params[:uuid].nil? or allowed_params[:lat].nil? or allowed_params[:lng].nil?
         render status: 400, json: { message: 'The request MUST contain a UUID, latitude and longitude.' }
         return false
