d73971550368a7aefa2dc528dcce43d55f2226c8
Gary Siu <Gary@untypeable.com>
2015-12-23 11:53:46 +0000

    Hooked up button to the new route

--- config/routes.rb
+++ config/routes.rb
@@ -80,6 +80,9 @@ Rails.application.routes.draw do
   namespace :developers do
     resource :console, only: [:show]
     resources :approvals, only: [:index, :new, :create]
+    # For cool API usage stats in the future
+    resources :requests, only: [:index]
+    put 'requests/pay', to: 'requests#pay'
   end
 
 
