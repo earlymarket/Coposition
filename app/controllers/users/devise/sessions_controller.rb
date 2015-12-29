3c7dc7b95cc1173c98a85c033f3d4b937d75b0ae
Dan Steele <danielsteele@hotmail.co.uk>
2015-12-29 13:51:16 +0000

    Making protection more specific

--- app/controllers/users/devise/sessions_controller.rb
+++ app/controllers/users/devise/sessions_controller.rb
@@ -1,7 +1,5 @@
 class Users::Devise::SessionsController < Devise::SessionsController
-
+  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format.symbol == :json }
   respond_to :json
-
-  protect_from_forgery with: :null_session
 
 end
