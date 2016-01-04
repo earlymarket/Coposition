7bf13937fc5ee3f90ae6d771b6898da9d7a36101
Gary Siu <Gary@untypeable.com>
2016-01-04 13:07:40 +0000

    Quick and dirty addition of checkins listing

--- app/controllers/api/v1/users/devices/checkins_controller.rb
+++ app/controllers/api/v1/users/devices/checkins_controller.rb
@@ -3,6 +3,20 @@ class Api::V1::Users::Devices::CheckinsController < Api::ApiController
 
   before_action :authenticate, :check_user_approved_developer, :find_device, :check_privilege
 
+  def index
+    checkins = @device.checkins.all
+    checkins = checkins.map do |checkin|
+      if params[:type] == "address"
+        checkin.reverse_geocode!
+        checkin = checkin.get_data
+      else
+        checkin = checkin.slice(:id, :uuid, :lat, :lng)
+      end
+    end
+
+    render json: checkins.to_json
+  end
+
   def last
     checkin = @device.checkins.last
     if params[:type] == "address"
