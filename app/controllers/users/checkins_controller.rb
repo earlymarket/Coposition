65fba5982bb1ea5f0f4dda254719888aebce24f0
t-harps <t.w.harps@gmail.com>
2016-01-05 11:58:11 +0000

    made show page better layout/show better variables

--- app/controllers/users/checkins_controller.rb
+++ app/controllers/users/checkins_controller.rb
@@ -3,8 +3,6 @@ class Users::CheckinsController < ApplicationController
   def show
     @checkin = Checkin.find(params[:id])
     @checkin.reverse_geocode!
-    @checkin.get_data
-    render @checkin
   end
 
 end
