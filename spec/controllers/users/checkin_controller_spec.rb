1a60dfddf3ef20483fad96b42ed1e9ad417d3f32
t-harps <t.w.harps@gmail.com>
2016-01-05 16:36:49 +0000

    Added test coverage of checkin_controller (show and delete)

--- spec/controllers/users/checkin_controller_spec.rb
+++ spec/controllers/users/checkin_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Users::DevicesController, type: :controller do
+RSpec.describe Users::CheckinsController, type: :controller do
   include ControllerMacros
 
-  let(:device) { FactoryGirl::create :device, user_id: user.id }
+  let(:device) do 
+    dev = FactoryGirl::create :device, user_id: user.id
+    dev.checkins << FactoryGirl::create(:checkin)
+    dev
+  end
   let(:user) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
     user = FactoryGirl.create(:user)
@@ -17,5 +21,56 @@ RSpec.describe Users::DevicesController, type: :controller do
     sign_in user
     user
   end
+  let(:checkin) do
+    check = FactoryGirl::create(:checkin)
+    device.checkins << check
+    check
+  end
 
+  describe 'GET #show' do
+    it 'should assign :id.checkin to @checkin if user owns device which owns checkin' do
+      get :show, {
+        user_id: user.username,
+        device_id: device.id,
+        id: checkin.id
+      }
+      expect(assigns :checkin).to eq(Checkin.find(checkin.id))
+    end
+
+    it 'should not assign :id.checkin if user does not own device which owns checkin' do
+      user
+      get :show, {
+        user_id: new_user.username,
+        device_id: device.id,
+        id: checkin.id
+      }
+      expect(assigns :checkin).to eq nil
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'should delete all checkins belonging to a device if user owns device' do
+      count = device.checkins.count
+      expect(count).to be > 0
+      delete :destroy, {
+        user_id: user.username,
+        device_id: device.id,
+        id: checkin.id
+      }
+      expect(device.checkins.count).to eq 0
+    end
+
+    it 'should not delete all checkins if user does not own device' do
+      user
+      checkin
+      count = device.checkins.count
+      expect(count).to be > 0
+      delete :destroy, {
+        user_id: new_user.username,
+        device_id: device.id,
+        id: checkin.id
+      }
+      expect(device.checkins.count).to eq count
+    end
+  end
 end
