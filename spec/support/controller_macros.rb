8edb4c718732f4f273d31e8c5076aaad5a889a5a
t-harps <t.w.harps@gmail.com>
2016-01-06 14:03:30 +0000

    removed empty line from controller macros

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -10,7 +10,6 @@ module ControllerMacros
   def res_hash
     # Check if it's a different request
     @json = nil if response != @res
-
     @json ||= begin
       json = JSON(response.body)
       json = JSON(response.body).symbolize_keys unless JSON(response.body).is_a? Array
