809aca8668a8494dba7aa0ad7aa6af02553a820c
t-harps <t.w.harps@gmail.com>
2016-01-08 16:09:17 +0000

    added redirect to approvals controller that sends user back to WFAT if they have already linked their account with Coposition

--- app/controllers/users/approvals_controller.rb
+++ app/controllers/users/approvals_controller.rb
@@ -5,6 +5,9 @@ class Users::ApprovalsController < ApplicationController
   def index
     @approved_devs = current_user.approved_developers
     @pending_approvals = current_user.pending_approvals
+    if @pending_approvals.count == 0 && params[:redirect]
+      redirect_to params[:redirect]
+    end
   end
 
   def approve
