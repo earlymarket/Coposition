e955134262766267ca117073d6daac47f5ce92cd
t-harps <t.w.harps@gmail.com>
2016-01-13 16:55:41 +0000

    method which returns a list of pending approvals

--- app/controllers/api/v1/users/approvals_controller.rb
+++ app/controllers/api/v1/users/approvals_controller.rb
@@ -7,16 +7,20 @@ class Api::V1::Users::ApprovalsController < Api::ApiController
   	# For some reason respond_with doesn't work here
   	# TODO: research why
     @dev.request_approval_from(@user).select(:id, :approved, :pending).first
-    approval = Approval.where(user: @user, developer: @dev)
-    render json: approval.to_json
+    @approval = Approval.where(user: @user, developer: @dev)
+    render json: @approval.to_json
   end
 
   def approve
-    @approval = Approval.where(id: params[:id], 
-      user: @user).first
+    @approval = Approval.where(id: params[:id], user: @user).first
     @approval.approve!
     @approved_devs = @user.approved_developers
     render nothing: true
+  end
+
+  def pending_approvals
+    @pending_approvals = @user.pending_approvals
+    render json: @pending_approvals
   end
 
   def status
