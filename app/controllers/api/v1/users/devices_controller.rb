11befe3fe0a18cc956d6dc93f7a44b1efc54354e
t-harps <t.w.harps@gmail.com>
2016-01-15 17:04:35 +0000

    removed find_user as already taking place in check_user_approved_developer

--- app/controllers/api/v1/users/devices_controller.rb
+++ app/controllers/api/v1/users/devices_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Users::DevicesController < Api::ApiController
   acts_as_token_authentication_handler_for User, only: [:switch_privilege_for_developer, :switch_all_privileges_for_developer]
 
   before_action :authenticate, :check_user_approved_developer
-  before_action :find_user, :check_user, only: [:switch_privilege_for_developer, :switch_all_privileges_for_developer]
+  before_action :check_user, only: [:switch_privilege_for_developer, :switch_all_privileges_for_developer]
 
   def index
     list = []
