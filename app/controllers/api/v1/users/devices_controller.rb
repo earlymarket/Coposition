5dedf068b855e1eb31dfe20dfd647afff2aca758
t-harps <t.w.harps@gmail.com>
2016-01-15 15:47:03 +0000

    adding privilege controls to api

--- app/controllers/api/v1/users/devices_controller.rb
+++ app/controllers/api/v1/users/devices_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::Users::DevicesController < Api::ApiController
   respond_to :json
 
+  acts_as_token_authentication_handler_for User, only: [:switch_privilege_for_developer, :switch_all_privileges_for_developer]
+
   before_action :authenticate, :check_user_approved_developer
+  before_action :find_user, only: [:switch_privilege_for_developer, :switch_all_privileges_for_developer]
 
   def index
     list = []
@@ -23,6 +26,35 @@ class Api::V1::Users::DevicesController < Api::ApiController
       end
     end
     respond_with list
+  end
+
+  def switch_privilege_for_developer
+    device = Device.where(id: params[:id], user: @user).first
+    developer = Developer.where(id: params[:developer_id]).first
+    if device && developer
+      device.change_privilege_for(developer, device.reverse_privilege_for(developer))
+      device.reverse_privilege_for(developer)
+      render status: 200, json: device.device_developer_privileges.where(developer: developer)
+    else
+      render status: 404, json: { message: 'Device/Developer not found' }
+    end
+  end
+
+  def switch_all_privileges_for_developer
+    devices = @user.devices
+    developer = Developer.where(id: params[:developer_id]).first
+    privileges = []
+    if devices && developer
+      devices.each do |device|
+        device.change_privilege_for(developer, device.reverse_privilege_for(developer))
+        device.privilege_for(developer)
+        device.reverse_privilege_for(developer)
+        privileges << device.device_developer_privileges.where(developer: developer)
+      end
+      render status: 200, json: privileges
+    else
+      render status: 404, json: { message: 'Device/Developer not found' }
+    end
   end
 
 end
