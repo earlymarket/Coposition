c6435e44fe69c304ce3e258c0809f13ec96e266b
t-harps <t.w.harps@gmail.com>
2016-01-19 11:10:07 +0000

    changed development server to use letter-opener rather than set up local env variables containing the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,17 +43,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => "coposition.dev@gmail.com",
-    :password             => "ZX12PO09",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :letter_opener
 
   Paperclip.options[:command_path] = "/usr/bin/"
 end
