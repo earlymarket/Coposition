3ec1b120808dee56af09848bdbe4d9272bbdb482
t-harps <t.w.harps@gmail.com>
2016-01-29 14:56:52 +0000

    updated API approvals controller soto integrate new functionality, temporary solution for checking where request from, params[origin: from-copo-app]

--- app/controllers/api/v1/users/approvals_controller.rb
+++ app/controllers/api/v1/users/approvals_controller.rb
@@ -7,17 +7,37 @@ class Api::V1::Users::ApprovalsController < Api::ApiController
   before_action :check_user, only: :update
 
   def create
-    Approval.link(@user, @dev, 'Developer')
-    approval = Approval.where(user: @user, approvable_id: @dev.id, approvable_type: 'Developer')
-    render json: approval
+    type = allowed_params[:approvable_type]
+    model = model_find(type)
+    approvable = model.find(allowed_params[:approvable])
+    if resource_exists?(type,approvable)
+      if (@user.friend_requests.include? approvable) || (@user.developer_requests.include? approvable)
+        #temporary if statement until we have a way of checking if request came from Copo app
+        if params[:origin] == 'from-copo-app'
+          Approval.accept(@user, approvable, type)
+        end
+      else
+        Approval.link(@user, approvable, type)
+        #temporary if statement until we have a way of checking if request came from Copo app
+        if params[:origin] == 'from-copo-app'
+          Approval.accept(@user, approvable, type) if type == 'Developer'
+        end
+      end
+      approval = Approval.where(user: @user, approvable: approvable, approvable_type: type)
+      render json: approval
+    end
   end
 
   def update
     approval = Approval.where(id: params[:id], user: @user).first
     if approval_exists? approval
-      approval.update(allowed_params)
-      @user.approve_devices(@dev) if allowed_params[:status] == 'accepted'
-      render json: approval
+      if allowed_params[:status] == 'accepted'
+        Approval.accept(@user, approval.approvable, approval.approvable_type)
+        render json: approval
+      else
+        approval.destroy
+        render status: 200, json: { message: 'Approval Destroyed' }
+      end
     end
   end
 
@@ -31,7 +51,7 @@ class Api::V1::Users::ApprovalsController < Api::ApiController
 
   private
     def allowed_params
-      params.require(:approval).permit(:status)
+      params.require(:approval).permit(:user, :approvable, :approvable_type, :status)
     end
 
     def check_user
--- spec/controllers/api/v1/users/approvals_controller_spec.rb
+++ spec/controllers/api/v1/users/approvals_controller_spec.rb
@@ -14,9 +14,30 @@ RSpec.describe Api::V1::Users::ApprovalsController, type: :controller do
   describe "a developer" do
 
     it "should be able to submit an approval request" do
-      post :create, user_id: user.username, format: :json
+      post :create, {
+        user_id: user.id,
+        approval: { 
+          approvable: developer.id,
+          approvable_type: 'Developer'
+        },
+        format: :json
+      } 
+      expect(Approval.first.status).to eq 'developer-requested'
+      expect(user.pending_approvals.count).to be 1
+    end
 
-      expect(response.status).to be 200
+    it "should be not be able to submit another request to same user" do
+      Approval.link(user,developer,'Developer')
+      post :create, {
+        user_id: user.id,
+        approval: { 
+          approvable: developer.id,
+          approvable_type: 'Developer'
+        },
+        format: :json
+      }
+      expect(Approval.count).to eq 1
+      expect(Approval.first.status).to eq 'developer-requested'
       expect(user.pending_approvals.count).to be 1
     end
 
@@ -24,10 +45,10 @@ RSpec.describe Api::V1::Users::ApprovalsController, type: :controller do
       # No approval
       get :status, user_id: user.username, format: :json
       expect(res_hash[:approval_status]).to be nil
+
       Approval.link(user,developer,'Developer')
       get :status, user_id: user.username, format: :json
       expect(res_hash[:approval_status]).to eq "developer-requested"
-
 
       Approval.accept(user,developer,'Developer')
       get :status, user_id: user.username, format: :json
@@ -52,60 +73,159 @@ RSpec.describe Api::V1::Users::ApprovalsController, type: :controller do
       request.headers["X-User-Email"] = user.email
     end
 
-    it "should be able to approve an approval" do
-      approval
-      put :update, {
-        user_id: user.id,
-        id: approval.id,
-        approval: {
-          status: 'accepted'
-        },
-        format: :json
-      }
-      expect(user.approved_developer? developer).to be true
+    context 'when post to create' do
+
+      it "should be able to create a developer approval" do
+        post :create, {
+          user_id: user.id,
+          approval: { 
+            approvable: developer.id,
+            approvable_type: 'Developer'
+          },
+          origin: 'from-copo-app',
+          format: :json
+        }
+        expect(Approval.last.user).to eq user
+        expect(Approval.last.approvable_id).to eq developer.id
+        expect(Approval.last.status).to eq 'accepted' 
+      end
+
+      it "should be able to create a user approval request" do
+        post :create, {
+          user_id: user.id,
+          approval: { 
+            approvable: second_user.id,
+            approvable_type: 'User'
+          },
+          format: :json
+        }
+        expect(Approval.count).to eq 2
+        expect(Approval.last.user).to eq second_user
+        expect(Approval.last.approvable_id).to eq user.id
+        expect(Approval.last.status).to eq 'requested' 
+      end
+
+      it "should be not be able to submit another request to same user" do
+        Approval.link(user,second_user,'User')
+        post :create, {
+          user_id: user.id,
+          approval: { 
+            approvable: second_user.id,
+            approvable_type: 'User'
+          },
+          format: :json
+        }
+        expect(Approval.count).to eq 2
+        expect(Approval.first.status).to eq 'pending'
+        expect(Approval.last.status).to eq 'requested'
     end
 
-    it "should not be able to approve another users approval (user_id)" do
-      second_user.approvals.create(approvable_id: developer.id)
-      approval
-      put :update, {
-        user_id: second_user.id,
-        id: approval.id,
-        approval: {
-          status: 'accepted'
-        },
-        format: :json
-      }
-      expect(response.status).to be 403
-      expect(user.approved_developer? developer).to be false
+      it "should approve a developer request" do
+        Approval.link(user,developer,'Developer')
+        expect(Approval.last.status).to eq 'developer-requested' 
+        post :create, {
+          user_id: user.id,
+          approval: { 
+            approvable: developer.id,
+            approvable_type: 'Developer'
+          },
+          origin: 'from-copo-app',
+          format: :json
+        }
+        expect(Approval.last.status).to eq 'accepted' 
+      end
+
+      it "should approve a friend request" do
+        Approval.link(second_user,user,'User')
+        expect(Approval.last.status).to eq 'requested' 
+        post :create, {
+          user_id: user.id,
+          approval: { 
+            approvable: second_user.id,
+            approvable_type: 'User'
+          },
+          origin: 'from-copo-app',
+          format: :json
+        }
+        expect(Approval.first.status).to eq 'accepted'       
+        expect(Approval.last.status).to eq 'accepted' 
+      end
     end
 
-    it "should not be able to approve an approval that does not exist/does not belong (approval_id)" do
-      second_user.approvals.create(approvable_id: developer.id)
-      approval
-      put :update, {
-        user_id: user.id,
-        id: second_user.approvals.last.id,
-        approval: {
-          status: 'accepted'
-        },
-        format: :json
-      }
-      expect(response.status).to be 404
-      expect(user.approved_developer? developer).to be false
-    end
+    context 'when posting to #update' do
 
-    it "should be able to reject an approval" do
-      approval
-      put :update, {
-        user_id: user.id,
-        id: approval.id,
-        approval: {
-          status: 'rejected'
-        },
-        format: :json
-      }
-      expect(user.approved_developer? developer).to be false
+      it "should be able to approve a developer approval request" do
+        approval
+        put :update, {
+          user_id: user.id,
+          id: approval.id,
+          approval: {
+            status: 'accepted'
+          },
+          format: :json
+        }
+        expect(user.approved_developer? developer).to be true
+      end
+
+      it "should be able to approve a user approval request" do
+        Approval.link(user, second_user, 'User')
+        expect(user.friends.include? second_user).to be false
+        put :update, {
+          user_id: user.id,
+          id: Approval.first,
+          approval: {
+            status: 'accepted'
+          },
+          format: :json
+        }
+        expect(second_user.friends.include? user).to be true
+        expect(user.friends.include? second_user).to be true
+      end
+
+      it "should not be able to approve another users approval (user_id)" do
+        second_user.approvals.create(approvable_id: developer.id)
+        approval
+        put :update, {
+          user_id: second_user.id,
+          id: approval.id,
+          approval: {
+            status: 'accepted'
+          },
+          format: :json
+        }
+        expect(response.status).to be 403
+        expect(user.approved_developer? developer).to be false
+      end
+
+      it "should not be able to approve an approval that does not exist/does not belong (approval_id)" do
+        second_user.approvals.create(approvable_id: developer.id)
+        approval
+        put :update, {
+          user_id: user.id,
+          id: second_user.approvals.last.id,
+          approval: {
+            status: 'accepted'
+          },
+          format: :json
+        }
+        expect(response.status).to be 404
+        expect(user.approved_developer? developer).to be false
+      end
+
+      it "should be able to reject an approval" do
+        approval
+        put :update, {
+          user_id: user.id,
+          id: approval.id,
+          approval: {
+            status: 'rejected'
+          },
+          format: :json
+        }
+        expect(Approval.count).to eq 0
+        expect(user.approved_developer? developer).to be false
+      end
+
     end
   end
 end
