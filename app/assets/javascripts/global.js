379a5ee320ae9835e4055fe75c04b84ab0097148
limzykenneth <limzy.kenneth@gmail.com>
2016-02-05 21:06:52 +0000

    Remove stray console.log

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,6 +2,5 @@
 $.fn.isOnScreen = function(){
     var element = this.get(0);
     var bounds = element.getBoundingClientRect();
-    console.log(bounds.top, bounds.bottom, window.innerHeight);
     return bounds.top < window.innerHeight && bounds.bottom > 0;
 };
