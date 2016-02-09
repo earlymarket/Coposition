5f2928d43fa9e38cceb4fde15ede010ca9216ab8
Gary Siu <Gary@untypeable.com>
2016-02-09 17:05:41 +0000

    Quick experiment with writing a helper

--- app/helpers/checkins_helper.rb
+++ app/helpers/checkins_helper.rb
@@ -1,2 +1,12 @@
 module CheckinsHelper
+
+  def checkin_fogged_icon(value)
+    if value
+      value = '<i class="material-icons">cloud_done</i>'
+    else
+      value = '<i class="material-icons">cloud_off</i>'
+    end
+    value.html_safe
+  end
+
 end
