e573630ab9246e127a00b432b091ebf6feeeed2a
t-harps <t.w.harps@gmail.com>
2016-02-10 10:53:03 +0000

    put permissions in a collapsible

--- app/models/device.rb
+++ app/models/device.rb
@@ -20,14 +20,6 @@ class Device < ActiveRecord::Base
     permissions.find_by(permissible_id: permissible.id, permissible_type: permissible.class.to_s).privilege
   end
 
-  def reverse_privilege_for(permissible)
-    if privilege_for(permissible) == "complete"
-      "disallowed"
-    else
-      "complete"
-    end
-  end
-
   def create_checkin(lat:, lng:)
     checkins << Checkin.create(uuid: uuid, lat: lat, lng: lng)
   end
