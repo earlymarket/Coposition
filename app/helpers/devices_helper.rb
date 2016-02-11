3a58ef74f99d40dbd6e9345edce92ab6b6d9942d
t-harps <t.w.harps@gmail.com>
2016-02-11 11:18:21 +0000

    removed some more unused bootstrap styling

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -1,23 +1,23 @@
 module DevicesHelper
   def devices_last_checkin(device)
     if device.checkins.exists?
-      message = "<p class='list-group-item-text'>Last reported in #{device.checkins.last.address}</p>"
+      message = "<p>Last reported in #{device.checkins.last.address}</p>"
     else
-      message = "<p class='list-group-item-text'>No Checkins found</p>"
+      message = "<p>No Checkins found</p>"
     end
     message.html_safe
   end
 
   def devices_fog_status(device)
     if device.fogged
-      message = "<p class='list-group-item-text'>Fogging is enabled on this device.</p>"
+      message = "<p>Fogging is enabled on this device.</p>"
       message.html_safe
     end
   end
 
   def devices_delay_status(device)
     if device.delayed
-      message = "<p class='list-group-item-text'>Timeshifted with a delay of #{device.delayed} minutes</p>"
+      message = "<p>Timeshifted with a delay of #{device.delayed} minutes</p>"
       message.html_safe
     end
   end
