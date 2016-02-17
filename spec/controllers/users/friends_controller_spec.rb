e1860095d1d865fc3b04b626ef01c08ad362d9d4
t-harps <t.w.harps@gmail.com>
2016-02-17 14:34:13 +0000

    deleted unused files

--- spec/controllers/users/friends_controller_spec.rb
+++ spec/controllers/users/friends_controller_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 RSpec.describe Users::FriendsController, type: :controller do
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
+
   end
 
 end
