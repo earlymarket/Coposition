33538ee5be61468c287a89f4a5365a09c162a3c7
t-harps <t.w.harps@gmail.com>
2016-02-19 14:40:53 +0000

    still flummoxed on how to get round checkins.flatten which slow the action down hugely

--- app/controllers/api/v1/users/checkins_controller.rb
+++ app/controllers/api/v1/users/checkins_controller.rb
@@ -67,8 +67,16 @@ class Api::V1::Users::CheckinsController < Api::ApiController
       if @device
         @device.checkins_for(@permissible)
       else
-        approval_date = @user.approval_for(@permissible).approval_date
-        @user.checkins.where("created_at > ?", approval_date)
+        # flatten takes ages, everything else is rapid
+        # alternative, only get since approval date for all devices:
+        # approval_date = user.approval_for(permissible).approval_date
+        # @user.checkins.where("created_at > ?", approval_date)
+        checkins = []
+        @user.devices.each do |device|
+          checkins << device.checkins_for(@permissible)
+        end
+        checkins = checkins.flatten
+        Checkin.where(id: checkins.map(&:id))
       end
     end
 
