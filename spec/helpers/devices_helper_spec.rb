3e83372795e5838d781a0806b71c947dc4655577
t-harps <t.w.harps@gmail.com>
2016-02-22 15:07:55 +0000

    merged dev, removed now unecessary tests

--- spec/helpers/devices_helper_spec.rb
+++ spec/helpers/devices_helper_spec.rb
@@ -18,22 +18,6 @@ RSpec.describe DevicesHelper, :type => :helper do
     end
   end
 
-  describe '#devices_fog_message' do
-    it "should return a string indicating fogging has been updated" do
-      expect(helper.devices_fog_message(device)).to match('no longer fogged')
-      device.fogged = true
-      expect(helper.devices_fog_message(device)).to match('has been fogged')
-    end
-  end
-
-  describe '#devices_delay_message' do
-    it "should return a string indicating delay has changed" do
-      expect(helper.devices_delay_message(device)).to match('not timeshifted')
-      device.delayed = Faker::Number.between(1, 300)
-      expect(helper.devices_delay_message(device)).to match(device.delayed.to_s)
-    end
-  end
-
   describe '#devices_fog_button_text' do
     it "should return a string for the fogging button" do
       expect(helper.devices_fog_button_text(device)).to eq('Fog')
