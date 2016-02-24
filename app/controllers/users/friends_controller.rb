3ec16ba7a0a131d62c53e8f8a5427c3df19c7a25
t-harps <t.w.harps@gmail.com>
2016-02-24 11:30:56 +0000

    updated the checkin table and added in a show checkin page

--- app/controllers/users/friends_controller.rb
+++ app/controllers/users/friends_controller.rb
@@ -5,7 +5,13 @@ class Users::FriendsController < ApplicationController
   end
 
   def show_device
-    @device = Device.find(params[:device_id])
+    @friend = current_user.friends.find(params[:id])
+    @device = @friend.devices.find(params[:device_id])
     @checkins = @device.checkins.order('created_at DESC').paginate(page: params[:page], per_page: 50)
+  end
+
+  def show_checkin
+    @friend = current_user.friends.find(params[:id])
+    @checkin = @friend.checkins.find(params[:checkin_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,7 @@ Rails.application.routes.draw do
     resources :friends, only: [:show] do
       member do
         get 'show_device'
+        get 'show_checkin'
       end
     end
   end
