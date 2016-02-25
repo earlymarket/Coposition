3edb8ed36b0380fac5962f5cea6291e44c3900b6
Gary Siu <Gary@untypeable.com>
2016-02-25 12:03:09 +0000

    Added composite index on city table. Should speed up .near queries a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209103131) do
+ActiveRecord::Schema.define(version: 20160225112906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20160209103131) do
     t.float  "longitude"
     t.string "country_code"
   end
+
+  add_index "cities", ["latitude", "longitude"], name: "index_cities_on_latitude_and_longitude", using: :btree
 
   create_table "developers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
