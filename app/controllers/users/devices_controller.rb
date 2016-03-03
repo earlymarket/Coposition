32134919aff8dadaab6140d791b5ba8bfa6a4e9c
t-harps <t.w.harps@gmail.com>
2016-03-03 10:44:33 +0000

    added some testing

--- app/controllers/users/devices_controller.rb
+++ app/controllers/users/devices_controller.rb
@@ -15,7 +15,10 @@ class Users::DevicesController < ApplicationController
 
   def show
     @device = Device.find(params[:id])
-    @checkins = Checkin.includes(:device).where(device_id: @device.id).order('created_at DESC').paginate(page: params[:page], per_page: 50)
+    @checkins = Checkin.includes(:device) \
+      .where(device_id: @device.id) \
+      .order('created_at DESC') \
+      .paginate(page: params[:page], per_page: 50)
   end
 
   def new
