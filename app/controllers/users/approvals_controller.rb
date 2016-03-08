87ca4991d6afc2ae6e0333caf0dc160d0cb68f58
Gary Siu <Gary@untypeable.com>
2016-03-08 10:52:57 +0000

    Put the logic in the model. We should probably remove the ApprovalMethods module

--- app/controllers/users/approvals_controller.rb
+++ app/controllers/users/approvals_controller.rb
@@ -27,10 +27,9 @@ class Users::ApprovalsController < ApplicationController
   end
 
   def apps
-    @apps = current_user.developers
-    @pending = Developer.joins(:approvals).where(approvals: {user_id: current_user.id}).where(approvals: {status: "developer-requested"})
+    @pending = current_user.developer_requests
     # Redirect if foreign app failed to create a pending approval.
-    if @apps.length == 0 && current_user.pending_approvals.length == 0 && params[:redirect]
+    if @pending.length == 0 && current_user.pending_approvals.length == 0 && params[:redirect]
       developer = Developer.find_by(api_key: params[:api_key])
       Approval.link(current_user, developer, 'Developer')
     elsif current_user.pending_approvals.length == 0 && params[:redirect]
