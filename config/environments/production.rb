7ec8a6c5a8b7318f89a3b20163e7aefdfcaf78af
t-harps <t.w.harps@gmail.com>
2016-03-09 12:49:34 +0000

    changed from .com to .net, hopefully this will fix sendgrid working on development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'coposition.com' }
+  config.action_mailer.default_url_options = { :host => 'coposition-pr-314.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.com",
+    address: "smtp.sendgrid.net",
     domain: "coposition-pr-314.herokuapp.com",
     port: 587,
     authentication: "plain",
