3a479015e47009317181cd6d479c3e7bf3fae393
t-harps <t.w.harps@gmail.com>
2016-03-10 17:19:17 +0000

    removed select box for privilege level, using two switches (disable access + last checkin only) instead, wrote some awful jquery to update correctly for the switches and also to disable the switches when disable access is on

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -2,12 +2,12 @@ module PermissionsHelper
 
   def permissible_title(permissible)
     if permissible.class.to_s == 'Developer'
-      title = "<div class='valign-wrapper col s5'>
+      title = "<div class='valign-wrapper col s8'>
       #{h image_tag(permissible.logo.url(:thumb), alt: '', class: 'circle icon')}
       #{h permissible.company_name}
       </div>"
     else
-      title = "<div class='valign-wrapper col s5'>#{h permissible.email}</div>"
+      title = "<div class='valign-wrapper col s8'>#{h permissible.email}</div>"
     end
     title.html_safe
   end
