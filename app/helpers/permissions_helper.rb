be10cb13df777897d7f750a40f7d5ef46fc598e8
Gary Siu <Gary@untypeable.com>
2016-03-17 12:08:11 +0000

    Added avatar to users/friends in the PermissionsHelper

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -7,7 +7,10 @@ module PermissionsHelper
       #{h permissible.company_name}
       </div>".html_safe
     else
-      "<div class='valign-wrapper col s8'>#{h permissible.email}</div>".html_safe
+      "<div class='valign-wrapper col s8'>
+      #{avatar_for(permissible, { size: '60x60', crop: :thumb, gravity: :face, radius: :max })}
+      #{h permissible.email}
+      </div>".html_safe
     end
   end
 
