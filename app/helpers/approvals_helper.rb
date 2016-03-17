11e5c73f7a385cb66776a4188f9d5a8b4c8e5796
t-harps <t.w.harps@gmail.com>
2016-03-17 10:57:10 +0000

    built a helper method to show who you have sent friend requests to, with correct commas/and

--- app/helpers/approvals_helper.rb
+++ app/helpers/approvals_helper.rb
@@ -7,4 +7,14 @@ module ApprovalsHelper
       { placeholder: "email@email.com", class: "validate", required: true }
     end
   end
+
+  def approvals_pending_friends(user)
+    string = ''
+    user.pending_friends.each_with_index do |friend, index|
+      string += friend.email
+      string += ', ' if index < user.pending_friends.length - 2
+      string += ' and ' if index == user.pending_friends.length - 2
+    end
+    return string
+  end
 end
--- spec/helpers/approvals_helper_spec.rb
+++ spec/helpers/approvals_helper_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ApprovalsHelper, :type => :helper do
+  let(:user) do
+    user = FactoryGirl::create(:user)
+    user.pending_friends << [FactoryGirl::create(:user), FactoryGirl::create(:user)]
+    user
+ end
+
   describe '#approvals_input' do
     it 'should assign placeholder key a string' do
       expect(helper.approvals_input('Developer')[:placeholder]).to match 'name'
@@ -10,6 +16,16 @@ RSpec.describe ApprovalsHelper, :type => :helper do
     it 'should assign class key' do
       expect(helper.approvals_input('User')[:class]).to match 'validate'
       expect(helper.approvals_input('Developer')[:class]).to match 'devs'
+    end
+  end
+
+  describe '#approvals_pending_friends' do
+    it 'should return a string with emails of users who requests sent to' do
+      expect(helper.approvals_pending_friends(user)).to be_kind_of(String)
+      expect(helper.approvals_pending_friends(user)).to_not match ','
+      expect(helper.approvals_pending_friends(user)).to match 'and'
+      user.pending_friends << FactoryGirl::create(:user)
+      expect(helper.approvals_pending_friends(user)).to match ','
     end
   end
 end
