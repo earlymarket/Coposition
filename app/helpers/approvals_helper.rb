cdbe6f2e74e1ad75213b40c7fe38e8c399af8770
Gary Siu <Gary@untypeable.com>
2016-03-17 19:44:22 +0000

    Hacky partial reuse with helpers. Should move to a presenter or something

--- app/helpers/approvals_helper.rb
+++ app/helpers/approvals_helper.rb
@@ -8,6 +8,22 @@ module ApprovalsHelper
     end
   end
 
+  def friend_or_app_name(entity)
+    if entity.respond_to? :username
+      friend = entity
+      friend.username.present? ? friend.username : friend.email.split("@").first
+    else
+      dev = entity
+      dev.company_name
+    end
+  end
+
+  def tagline_text(entity)
+    if entity.respond_to? :tagline
+      if entity.tagline then "<div>#{entity.tagline}</div>".html_safe end
+    end
+  end
+
   def approvals_pending_friends(user)
     string = ''
     user.pending_friends.each_with_index do |friend, index|
