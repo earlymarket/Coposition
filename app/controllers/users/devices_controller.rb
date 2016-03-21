e7e9c6ccf2b282ef64ee5aec5d659fe6727078a7
Gary Siu <Gary@untypeable.com>
2016-03-21 10:42:02 +0000

    Re-enabled Bullet. Got rid of unused eagerload

--- app/controllers/users/devices_controller.rb
+++ app/controllers/users/devices_controller.rb
@@ -15,7 +15,7 @@ class Users::DevicesController < ApplicationController
 
   def show
     @device = Device.find(params[:id])
-    @checkins = Checkin.includes(:device) \
+    @checkins = Checkin \
       .where(device_id: @device.id) \
       .order('created_at DESC') \
       .paginate(page: params[:page], per_page: 50)
