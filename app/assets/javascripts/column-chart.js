480acad50d23ad8bf6dbfd9dd7c098d55f382a07
t-harps <t.w.harps@gmail.com>
2016-03-21 15:42:57 +0000

    added fogging link

--- app/assets/javascripts/column-chart.js
+++ app/assets/javascripts/column-chart.js
@@ -18,27 +18,27 @@ window.COPO.charts = {
     function selectHandler() {
       var selectedItem = chart.getSelection()[0];
       if (selectedItem) {
-        var splitDate = gon.chart_checkins[selectedItem.row][0].split("/");
+        var splitColumnDate = gon.chart_checkins[selectedItem.row][0].split("/");
         gon.table_checkins = [];
-        if (splitDate.length === 3){
-          var columnDate = new Date(splitDate[2], splitDate[1]-1, splitDate[0]);
-          var startMSeconds = Date.parse(columnDate);
-          var endMSeconds = startMSeconds + 86400000; //one day
-          gon.checkins.forEach(function(e){
-            var dateMSeconds = Date.parse(e.created_at);
-            if (dateMSeconds >= startMSeconds && dateMSeconds <= endMSeconds){
-              gon.table_checkins.push(e);
+
+        if (splitColumnDate.length === 3){
+          var columnDate = new Date(splitColumnDate[2], splitColumnDate[1]-1, splitColumnDate[0]);
+          gon.checkins.forEach(function(checkin){
+            date = new Date(new Date(checkin.created_at).setHours(0,0,0,0));
+            if (date.toString() === columnDate.toString()){
+              gon.table_checkins.push(checkin);
             }
           })
-        } else if (splitDate.length ===2) {
-          gon.checkins.forEach(function(e){
-            var month = new Date(e.created_at).getMonth();
-            var year = new Date(e.created_at).getFullYear().toString();
-            if (splitDate[0]-1 == month && splitDate[1] == year.substr(year.length-2)){
-              gon.table_checkins.push(e);
+        } else if (splitColumnDate.length ===2) {
+          gon.checkins.forEach(function(checkin){
+            var month = new Date(checkin.created_at).getMonth();
+            var year = new Date(checkin.created_at).getFullYear().toString();
+            if (month == splitColumnDate[0]-1 && year.substr(year.length-2) == splitColumnDate[1]){
+              gon.table_checkins.push(checkin);
             }
           })
         }
+
         COPO.charts.drawTable();
       }
     }
@@ -52,19 +52,23 @@ window.COPO.charts = {
   drawTable: function() {
     // Define the chart to be drawn.
     var tableData = [];
-    gon.table_checkins.forEach(function(e){
-      var humanizedDate = new Date(e.created_at).toLocaleDateString('en-GB')
-      tableData.push([humanizedDate, e.fogged_area]);
+    gon.table_checkins.forEach(function(checkin){
+      var humanizedDate = new Date(checkin.created_at).toLocaleDateString('en-GB')
+      var fogging = COPO.utility.ujsLink('put', '<i class="material-icons">cloud</i>' , window.location.pathname + '/checkins/' + checkin.id )[0];
+      tableData.push([humanizedDate, checkin.fogged_area, fogging.outerHTML]);
     })
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Created');
     data.addColumn('string', 'Area');
+    data.addColumn('string', 'Fogging');
+    data.setColumnProperty(2, {allowHtml: true})
     data.addRows(tableData);
 
     // Instantiate and draw the chart.
     var table = new google.visualization.Table(document.getElementById('table-chart'));
-    var options = { width: '100%', height: '100%' }
+    var options = { width: '100%', height: '100%', allowHtml: true }
     table.draw(data, options);
   }
 }
+
 
