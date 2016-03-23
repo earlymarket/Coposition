3d40382d77ca2d07f91ea7ee68f513be2070d630
t-harps <t.w.harps@gmail.com>
2016-03-23 12:53:16 +0000

    created functions for building table checkins on column selection, split lines out for delete/fogging buttons

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -15,7 +15,6 @@ window.COPO.charts = {
       legend: {position: 'none'}
     };
 
-
     function selectHandler() {
       if (chart.getSelection().length === 0){
         gon.table_checkins = gon.checkins;
@@ -25,25 +24,33 @@ window.COPO.charts = {
           var splitColumnDate = gon.chart_checkins[selectedItem.row][0].split("/");
           gon.table_checkins = [];
           if (splitColumnDate.length === 3){
-            var columnDate = new Date(splitColumnDate[2], splitColumnDate[1]-1, splitColumnDate[0]);
-            gon.checkins.forEach(function(checkin){
-              date = new Date(new Date(checkin.created_at).setHours(0,0,0,0));
-              if (date.toString() === columnDate.toString()){
-                gon.table_checkins.push(checkin);
-              }
-            })
+            day_table_checkins(splitColumnDate);
           } else if (splitColumnDate.length ===2) {
-            gon.checkins.forEach(function(checkin){
-              var month = new Date(checkin.created_at).getMonth();
-              var year = new Date(checkin.created_at).getFullYear().toString();
-              if (month === splitColumnDate[0]-1 && year.substr(year.length-2) === splitColumnDate[1]){
-                gon.table_checkins.push(checkin);
-              }
-            })
+            month_table_checkins(splitColumnDate);
           }
         }
       }
       COPO.charts.drawTable();
+    }
+
+    function day_table_checkins(splitColumnDate) {
+      var columnDate = new Date(splitColumnDate[2], splitColumnDate[1]-1, splitColumnDate[0]);
+      gon.checkins.forEach(function(checkin){
+        date = new Date(new Date(checkin.created_at).setHours(0,0,0,0));
+        if (date.toString() === columnDate.toString()){
+          gon.table_checkins.push(checkin);
+        }
+      })
+    }
+
+    function month_table_checkins(splitColumnDate) {
+      gon.checkins.forEach(function(checkin){
+        var month = new Date(checkin.created_at).getMonth();
+        var year = new Date(checkin.created_at).getFullYear().toString();
+        if (month === splitColumnDate[0]-1 && year.substr(year.length-2) === splitColumnDate[1]){
+          gon.table_checkins.push(checkin);
+        }
+      })
     }
 
     // Listen for the 'select' event, and call my function selectHandler() when
@@ -59,8 +66,16 @@ window.COPO.charts = {
       var humanizedDate = new Date(checkin.created_at).toLocaleDateString('en-GB');
       var foggedClass;
       checkin.fogged ? foggedClass = 'fogged enabled-icon' : foggedClass = ' disabled-icon';
-      var delete_button = COPO.utility.ujsLink('delete', '<i class="material-icons red-text right">delete_forever</i>' , window.location.pathname + '/checkins/' + checkin.id ).attr('data-confirm', 'Are you sure?').prop('outerHTML')
-      var fogging_button = COPO.utility.ujsLink('put', '<i class="material-icons">cloud</i>' , window.location.pathname + '/checkins/' + checkin.id ).attr('id', 'tableFog' + checkin.id).attr('class', foggedClass).prop('outerHTML')
+      var delete_button = COPO.utility.ujsLink('delete',
+        '<i class="material-icons red-text right">delete_forever</i>' ,
+        window.location.pathname + '/checkins/' + checkin.id )
+        .attr('data-confirm', 'Are you sure?')
+        .prop('outerHTML')
+      var fogging_button = COPO.utility.ujsLink('put',
+        '<i class="material-icons">cloud</i>' ,
+        window.location.pathname + '/checkins/' + checkin.id )
+        .attr('id', 'tableFog' + checkin.id).attr('class', foggedClass)
+        .prop('outerHTML')
       tableData.push([humanizedDate, checkin.fogged_area, fogging_button, delete_button]);
     })
     var data = new google.visualization.DataTable();
