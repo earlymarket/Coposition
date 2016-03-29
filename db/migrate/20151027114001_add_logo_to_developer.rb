f79c81fd284c8eaa96c7b2acf3eddb014fb2f690
Gary Siu <Gary@untypeable.com>
2016-03-29 17:05:18 +0100

    Commited the cardinal sin of revising history to remove the migration

--- db/migrate/20151027114001_add_logo_to_developer.rb
+++
@@ -1,9 +0,0 @@
-class AddLogoToDeveloper < ActiveRecord::Migration
-  def up
-    add_attachment :developers, :logo
-  end
-
-  def down
-    remove_attachment :developers, :logo
-  end
-end
--- db/migrate/20160324142829_remove_paperclip_from_developers.rb
+++
@@ -1,8 +0,0 @@
-class RemovePaperclipFromDevelopers < ActiveRecord::Migration
-  def change
-    remove_column(:developers, :logo_file_name, :string)
-    remove_column(:developers, :logo_content_type, :string)
-    remove_column(:developers, :logo_file_size, :integer)
-    remove_column(:developers, :logo_updated_at, :datetime)
-  end
-end
