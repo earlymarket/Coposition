945f2cac3cc4e746d3081dc24ee360ab0b083b4f
t-harps <t.w.harps@gmail.com>
2016-03-30 10:01:10 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -88,10 +88,6 @@ ActiveRecord::Schema.define(version: 20160316121833) do
     t.datetime "updated_at",                          null: false
     t.string   "api_key"
     t.string   "company_name",                        null: false
-    t.string   "logo_file_name"
-    t.string   "logo_content_type"
-    t.integer  "logo_file_size"
-    t.datetime "logo_updated_at"
     t.string   "tagline"
     t.string   "redirect_url"
   end
