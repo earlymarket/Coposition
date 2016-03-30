9523769b26026ed701e97487fe43aeea5b056580
Gary Siu <Gary@untypeable.com>
2016-03-30 17:44:45 +0100

    Commented out friend/app request flash notifications. Added badges for them in nav.

--- app/controllers/users/dashboards_controller.rb
+++ app/controllers/users/dashboards_controller.rb
@@ -3,7 +3,7 @@ class Users::DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    flash[:notification] = current_user.notifications
+    # flash[:notification] = current_user.notifications
   end
 
 end
---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,7 @@
+module NavbarHelper
+
+  def navbar_badge(text, count)
+    count > 0 ? text.html_safe + "<span class=\"new badge\">#{count}</span>".html_safe : text.html_safe
+  end
+
+end
