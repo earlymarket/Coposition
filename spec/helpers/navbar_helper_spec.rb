93e3e19ca0d9c50c1acc718a4b7dae3c53ae05f6
Gary Siu <Gary@untypeable.com>
2016-03-31 18:17:55 +0100

    Randomized the spec a bit

--- spec/helpers/navbar_helper_spec.rb
+++ spec/helpers/navbar_helper_spec.rb
@@ -2,16 +2,19 @@ require "rails_helper"
 
 RSpec.describe NavbarHelper, :type => :helper do
 
+  let(:random_word) { Faker::Lorem.word }
+  let(:random_count) { Faker::Number.between(1, 99) }
+
   describe '#navbar_indicator' do
     it 'should insert an indicator if given a count greater than 0' do
-      expect { helper.navbar_indicator('blah', 1) }.not_to raise_error
-      expect(helper.navbar_indicator('blah', 1)).to match('blah')
-      expect(helper.navbar_indicator('blah', 1).length).to be > 'blah'.length
+      expect { helper.navbar_indicator(random_word, random_count) }.not_to raise_error
+      expect(helper.navbar_indicator(random_word, random_count)).to match(random_word)
+      expect(helper.navbar_indicator(random_word, random_count).length).to be > random_word.length
     end
 
     it 'should return the input string if count is 0' do
-      expect { helper.navbar_indicator('blah', 0) }.not_to raise_error
-      expect(helper.navbar_indicator('blah', 0)).to eq('blah')
+      expect { helper.navbar_indicator(random_word, 0) }.not_to raise_error
+      expect(helper.navbar_indicator(random_word, 0)).to eq(random_word)
     end
   end
 
