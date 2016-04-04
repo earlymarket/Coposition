6185231829ba1f1381f76b0db25fa6f7a5d8d234
t-harps <t.w.harps@gmail.com>
2016-04-04 10:53:12 +0100

    removed new state and changed new_state function to just get new privilege level, renamed

--- app/assets/javascripts/permissions.js
+++ app/assets/javascripts/permissions.js
@@ -16,9 +16,12 @@ window.COPO.permissions = {
 
       var permission_id =  $(this).data().permission;
       var permission = _.find(gon.permissions, _.matchesProperty('id', permission_id));
-      var new_state = permission[attribute] = COPO.permissions.new_state(permission[attribute], switch_type);
       var device_id = permission['device_id'];
-
+      if (permission[attribute].constructor === Boolean){
+        permission[attribute] = !permission[attribute]
+      } else {
+        permission[attribute] = COPO.permissions.new_privilege(permission[attribute], switch_type);
+      }
       if (switch_type === "disallowed") {
         $("div[data-permission='"+permission_id+"'][data-switch=last_only]").find('input').prop("checked", false);
         COPO.permissions.toggle_switches_disabled(permission_id);
@@ -27,7 +30,7 @@ window.COPO.permissions = {
       $.ajax({
         url: "/users/"+gon.current_user_id+"/devices/"+device_id+"/permissions/"+permission_id+"",
         type: 'PUT',
-        data: { permission : {[attribute]: new_state} }
+        data: { permission }
       });
     })
   },
@@ -37,17 +40,15 @@ window.COPO.permissions = {
     element.prop("disabled", !element.prop("disabled"));
   },
 
-  new_state: function(current_state, switch_type){
-    if(current_state === "disallowed"){
+  new_privilege: function(current_privilege, switch_type){
+    if(current_privilege === "disallowed"){
       return "complete"
     } else if(switch_type === "disallowed"){
       return "disallowed"
-    } else if(current_state === "complete"){
+    } else if(current_privilege === "complete"){
       return "last_only"
-    } else if(current_state === "last_only"){
+    } else if(current_privilege === "last_only"){
       return "complete"
-    } else {
-      return !current_state
     }
   }
 };
