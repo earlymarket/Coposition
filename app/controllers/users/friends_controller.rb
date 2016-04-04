daae3fc8ea154af5606e2bba4eab9c5608dd51fe
t-harps <t.w.harps@gmail.com>
2016-04-04 14:01:53 +0100

    got rid of unecessary variable assignments, just using gon variables

--- app/controllers/users/friends_controller.rb
+++ app/controllers/users/friends_controller.rb
@@ -9,13 +9,11 @@ class Users::FriendsController < ApplicationController
   def show_device
     @friend = User.find(params[:id])
     @device = @friend.devices.find(params[:device_id])
-    @paginated_checkins = @friend.get_checkins(current_user, @device).order(created_at: :desc) \
+    gon.checkins = @friend.get_checkins(current_user, @device).order(created_at: :desc) \
       .paginate(page: params[:page], per_page: 1000)
-    @checkins = @paginated_checkins
-    @checkins = @checkins.map do |checkin|
+    gon.checkins = gon.checkins.map do |checkin|
       checkin.get_data
     end unless @device.can_bypass_fogging?(current_user)
-    gon.checkins = @paginated_checkins
   end
 
   private
