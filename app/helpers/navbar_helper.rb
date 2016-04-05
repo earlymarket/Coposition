b66a3045673e8f9a6a1ba9730e1fd8ba1c8ea61a
t-harps <t.w.harps@gmail.com>
2016-04-05 11:36:10 +0100

    removing indicator to see if this has any effect

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,7 +1,7 @@
 module NavbarHelper
 
   def navbar_indicator(text, count)
-    count > 0 ? text.html_safe + ' <div class="indicator"></div>'.html_safe : text
+    count > 0 ? text.html_safe + ' <div class=""></div>'.html_safe : text
   end
 
 end
