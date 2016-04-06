3884d0b2dc7af3f1834a7bf0a67bacf4335c9c28
t-harps <t.w.harps@gmail.com>
2016-04-06 16:00:37 +0100

    made sure address is only replaced if geocoding returns an address

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -86,8 +86,10 @@ window.COPO.maps = {
           url: "/users/"+gon.current_user_id+"/devices/"+checkin.device_id+"/checkins/"+checkin.id,
           dataType: "json"
         }).done(function(data) {
-          $geocodedAddress = '<li class="address">Address: ' + data.address + '</li>'
-          $('.address').replaceWith($geocodedAddress);
+          if (data.address != null){
+            $geocodedAddress = '<li class="address">Address: ' + data.address + '</li>'
+            $('.address').replaceWith($geocodedAddress);
+          }
         })
       }
       map.panTo(this.getLatLng());
