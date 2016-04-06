ffd253e02dab926050815643eb3a7f63d3fbb048
t-harps <t.w.harps@gmail.com>
2016-04-06 13:06:03 +0100

    kept in all changes but changed the death line to a version that i think will work

--- app/assets/javascripts/permissions.js
+++ app/assets/javascripts/permissions.js
@@ -11,41 +11,29 @@ window.COPO.permissions = {
 
   switch_change:function(){
     $(".switch").change(function( event ) {
-      var permission_id = $(this).data().permission;
       var attribute = $(this).data().attribute;
       var switch_type = $(this).data().switch;
-
-      var permission = $.grep(gon.permissions, function(perm){ return perm.id === permission_id; });
-      permission = permission[0];
-      permission[attribute] = COPO.permissions.new_state(permission[attribute], switch_type);
+      var permission_id =  $(this).data().permission;
+      var permission = _.find(gon.permissions, _.matchesProperty('id', permission_id));
       var device_id = permission['device_id'];
+
+      if (permission[attribute].constructor === Boolean){
+        permission[attribute] = !permission[attribute]
+      } else {
+        permission[attribute] = COPO.permissions.new_privilege(permission[attribute], switch_type);
+      }
 
       if (switch_type === "disallowed") {
         $("div[data-permission='"+permission_id+"'][data-switch=last_only]").find('input').prop("checked", false);
         COPO.permissions.toggle_switches_disabled(permission_id);
       }
 
-      var data = COPO.permissions.set_data(attribute, permission[attribute]);
       $.ajax({
         url: "/users/"+gon.current_user_id+"/devices/"+device_id+"/permissions/"+permission_id+"",
         type: 'PUT',
-        data: { permission }
+        data: { permission: permission }
       });
     })
-  },
-
-  new_state: function(current_state, switch_type){
-    if(current_state === "disallowed"){
-      return "complete"
-    } else if(switch_type === "disallowed"){
-      return "disallowed"
-    } else if(current_state === "complete"){
-      return "last_only"
-    } else if(current_state === "last_only"){
-      return "complete"
-    } else {
-      return !current_state
-    }
   },
 
   toggle_switches_disabled: function(permission_id){
@@ -53,14 +41,15 @@ window.COPO.permissions = {
     element.prop("disabled", !element.prop("disabled"));
   },
 
-  set_data: function(attribute, value){
-    if (attribute === 'privilege'){
-      return { privilege: value };
-    } else if (attribute === 'bypass_fogging'){
-      return { bypass_fogging: value };
-    } else {
-      return { bypass_delay: value };
+  new_privilege: function(current_privilege, switch_type){
+    if(current_privilege === "disallowed"){
+      return "complete"
+    } else if(switch_type === "disallowed"){
+      return "disallowed"
+    } else if(current_privilege === "complete"){
+      return "last_only"
+    } else if(current_privilege === "last_only"){
+      return "complete"
     }
   }
 };
-
