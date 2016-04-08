8b531425bd0b20840f82229d1162e912bd99437d
t-harps <t.w.harps@gmail.com>
2016-04-08 15:28:12 +0100

    added setOnLoad callback function as chart was breaking and removed old c3 d3 code

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,59 +1,10 @@
 $(document).on('page:change', function() {
   if ($(".c-dashboards.a-show").length === 1) {
-    COPO.charts.drawBarChart(gon.checkins, '270');
+    google.charts.setOnLoadCallback(function() {
+       COPO.charts.drawBarChart(gon.weeks_checkins, '270');
+    });
     $(window).resize(function(){
-      COPO.charts.drawBarChart(gon.checkins, '270');
-     });
-    var checkinData =  {
-      x: 'x',
-      columns: [
-        ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-        ['device-1', 30, 200, 100, 400, 150, 250],
-        ['device-2', 50, 20, 10, 40, 15, 25]
-      ],
-
-      type: 'bar',
-
-      groups: [
-        ['device-1', 'device-2']
-      ]
-    };
-
-    var checkinAxes = {
-      y: {
-        label: {
-          text: 'Check-ins',
-          position: 'outer-middle'
-        }
-      },
-
-      x: {
-        type: 'timeseries',
-        tick: {
-            format: '%m/%d'
-        },
-        label: {
-          text: 'Dates',
-          position: 'outer-center'
-        }
-      }
-    };
-
-    var dailyCheckins = c3.generate({
-      bindto: '#dashboard-charts #daily-checkins div',
-      data: checkinData,
-      axis: checkinAxes,
-      bar: {
-        width: {
-          ratio: 0.9
-        }
-      },
-      zoom: {
-        enabled: true
-      },
-      color: {
-          pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
-      }
+      COPO.charts.drawBarChart(gon.weeks_checkins, '270');
     });
   }
 });
--- app/controllers/users/dashboards_controller.rb
+++ app/controllers/users/dashboards_controller.rb
@@ -3,16 +3,17 @@ class Users::DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    checkins = current_user.get_user_checkins(nil)
-    fogged_area_count = checkins.hash_group_and_count_by(:fogged_area)
-    device_checkins_count = checkins.hash_group_and_count_by(:device_id)
-    @most_used_device = Device.find(device_checkins_count.first.first)
-    @most_frequent = fogged_area_count.first 5
-    gon.checkins = checkins.where(created_at: 1.week.ago..Time.now)
-    @week_checkins_count = gon.checkins.count
-    last_weeks_checkins_count = checkins.where(created_at: 2.weeks.ago..1.week.ago).count
-    if @week_checkins_count > 0 && last_weeks_checkins_count > 0
-      @percent_change = (((@week_checkins_count.to_f/last_weeks_checkins_count.to_f)-1)*100).round(2)
+    if (checkins = current_user.get_user_checkins(nil)).present?
+      gon.weeks_checkins = checkins.where(created_at: 1.week.ago..Time.now)
+      fogged_area_count = checkins.hash_group_and_count_by(:fogged_area)
+      device_checkins_count = checkins.hash_group_and_count_by(:device_id)
+      @most_used_device = Device.find(device_checkins_count.first.first)
+      @most_frequent = fogged_area_count.first 5
+      @week_checkins_count = gon.weeks_checkins.count
+      last_weeks_checkins_count = checkins.where(created_at: 2.weeks.ago..1.week.ago).count
+      if @week_checkins_count > 0 && last_weeks_checkins_count > 0
+        @percent_change = (((@week_checkins_count.to_f/last_weeks_checkins_count.to_f)-1)*100).round(2)
+      end
     end
   end
 
