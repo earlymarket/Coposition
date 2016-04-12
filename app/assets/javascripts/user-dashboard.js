168c096072f13123ae3663980598fd99108c4409
Gary Siu <Gary@untypeable.com>
2016-04-12 19:00:55 +0100

    Renamed dashboard.js for consistency. Init and fit the map.

--- app/assets/javascripts/user-dashboard.js
+++ app/assets/javascripts/user-dashboard.js
@@ -1,5 +1,8 @@
 $(document).on('page:change', function() {
   if ($(".c-dashboards.a-show").length === 1) {
+
+    COPO.maps.initMap();
+    map.fitWorld();
     google.charts.setOnLoadCallback(function() {
        COPO.charts.drawBarChart(gon.weeks_checkins, '270');
     });
