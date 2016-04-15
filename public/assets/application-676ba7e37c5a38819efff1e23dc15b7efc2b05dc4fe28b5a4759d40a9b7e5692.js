7cad8ee8947997e8e893eaadc21e2c01efce8984
t-harps <t.w.harps@gmail.com>
2016-04-15 09:30:32 +0100

    assets

--- public/assets/application-676ba7e37c5a38819efff1e23dc15b7efc2b05dc4fe28b5a4759d40a9b7e5692.js
+++ public/assets/application-676ba7e37c5a38819efff1e23dc15b7efc2b05dc4fe28b5a4759d40a9b7e5692.js
@@ -52186,6 +52186,11 @@ window.COPO.slider = {
 $(document).on('page:change', function() {
   if ($(".c-approvals").length === 1) {
     $('.tooltipped').tooltip({delay: 50});
+    COPO.permissions.set_masters('approved');
+    COPO.permissions.master_change('approved');
+    COPO.permissions.switch_change('approved');
+    COPO.permissions.check_disabled();
+    COPO.permissions.check_bypass();
   }
 })
 ;
