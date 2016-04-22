3ad2c58cccc66abe3c5a68dc37a3a16a249b616c
t-harps <t.w.harps@gmail.com>
2016-04-22 14:32:18 +0100

    added tests for changing permissions from the Apps page

--- features/step_definitions/permissions_steps.rb
+++ features/step_definitions/permissions_steps.rb
@@ -26,3 +26,21 @@ Given(/^I should have privilege set to "(.*?)"$/) do |value|
   sleep 0.5
   expect(Permission.last.privilege).to eq value
 end
+
+Given(/^I change my permissions$/) do
+  steps %Q{
+    Given I click the link "lock"
+      And I click the switch "bypass-fogging"
+      And I click the switch "bypass-delay"
+      And I click the switch "last-only"
+        Then I should have "bypass_fogging" enabled
+        And I should have "bypass_delay" enabled
+        And I should have privilege set to "last_only"
+      When I click the switch "disallowed"
+        Then I should have privilege set to "disallowed"
+      And I click the switch "last-only"
+      And I click the switch "bypass-fogging"
+        Then I should have privilege set to "disallowed"
+        And I should have "bypass_delay" enabled
+  }
+end
