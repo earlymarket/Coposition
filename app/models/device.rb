09d9496304fe8c44a17f904fe04a5578f7343e0c
t-harps <t.w.harps@gmail.com>
2016-04-25 16:17:37 +0100

    added conditional to device name uniqueness check so that requesting a uuid works

--- app/models/device.rb
+++ app/models/device.rb
@@ -8,7 +8,7 @@ class Device < ActiveRecord::Base
   has_many :developers, through: :permissions, source: :permissible, :source_type => "Developer"
   has_many :permitted_users, through: :permissions, source: :permissible, :source_type => "User"
 
-  validates :name, uniqueness: { scope: :user_id }
+  validates :name, uniqueness: { scope: :user_id }, if: :user_id
 
   before_create do |dev|
     dev.uuid = SecureRandom.uuid
