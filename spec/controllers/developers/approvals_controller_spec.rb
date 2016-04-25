ee8120d21bd03abb21f0b35cf90f671fd4fe8bef
t-harps <t.w.harps@gmail.com>
2016-04-25 17:39:35 +0100

    began writing tests for untested developer namespaced controllers

---
+++ spec/controllers/developers/approvals_controller_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe Developers::ApprovalsController, type: :controller do
+  include ControllerMacros
+
+  let(:developer) { create_developer }
+  let(:user) {FactoryGirl::create :user}
+  let(:second_user) {FactoryGirl::create :user}
+  let(:approval) do
+    app = FactoryGirl::create :approval
+    app.update(approvable: developer, user: user, approvable_type: 'Developer')
+    app
+  end
+  let(:second_approval) do
+    app = FactoryGirl::create :approval
+    app.update(approvable: developer, user: second_user, approvable_type: 'Developer')
+    app
+  end
+  let(:developer_params) {{ developer_id: developer.id }}
+  let(:approval_create_params) do
+   developer_params.merge(approval: { user: user.email, approvable_type: 'Developer' })
+  end
+
+  describe '#index' do
+    it 'should show amount of pending users and show approved users' do
+      approval.update(user: user, status: 'accepted')
+      second_approval.update(user: second_user, status: 'developer-requested')
+      get :index, developer_params
+      expect(assigns(:pending)).to eq developer.pending_approvals
+      expect(assigns(:users)).to eq developer.users
+    end
+  end
+
+  describe '#new' do
+    it 'should assign an approval' do
+      get :new, developer_params
+      expect((assigns :approval).class).to eq Approval.new.class
+    end
+  end
+
+  describe '#create' do
+    it 'should create an approval between user and developer' do
+      post :create, approval_create_params
+      expect(Approval.last.approvable_id).to eq developer.id
+      expect(Approval.last.status).to eq 'developer-requested'
+      expect(Approval.last.user).to eq user
+      expect(flash[:notice]).to match 'Successfully sent'
+    end
+
+    it 'should fail to create an approval if request exists' do
+      approval.update(user: user, status: 'accepted')
+      post :create, approval_create_params
+      expect(flash[:alert]).to match 'Error creating approval'
+      expect(Approval.count).to eq 1
+    end
+
+    it 'should fail to create an approval if user does not exist' do
+      approval_create_params[:approval].merge!(user: 'does not exist')
+      post :create, approval_create_params
+      expect(flash[:alert]).to match 'Error creating approval'
+      expect(Approval.count).to eq 0
+    end
+  end
+end
