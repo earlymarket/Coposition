aab4212c1cbce673dd442fa7a8ad6f37e579ca5a
t-harps <t.w.harps@gmail.com>
2016-04-27 15:14:48 +0100

    removing sleep as its bad apparently, using have_selector which using default wait time

--- features/step_definitions/checkin_steps.rb
+++ features/step_definitions/checkin_steps.rb
@@ -1,14 +1,10 @@
 Then(/^I have "(.*?)" checkins on the map$/) do |number|
-  sleep 1
-  expect(all("img.leaflet-marker-icon").size).to be number.to_i
-  expect(@me.checkins.count).to be number.to_i
+  expect(page).to have_selector("img.leaflet-marker-icon", count: number.to_i)
 end
 
 Then(/^I have "(.*?)" checkins in the table$/) do |number|
-  sleep 0.5
   # +1 as the table headers count as another tr
-  expect(all("tr").size).to be number.to_i+1
-  expect(@me.checkins.count).to be number.to_i
+  expect(page).to have_selector("tr", count: number.to_i+1)
 end
 
 Given(/^I click on my last checkin$/) do
