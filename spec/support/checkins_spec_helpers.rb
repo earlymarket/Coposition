c9226171f746b3eb5f98fa865d66fcee77ab9469
t-harps <t.w.harps@gmail.com>
2016-04-28 17:01:49 +0100

    removed params and call_index method

--- spec/support/checkins_spec_helpers.rb
+++ spec/support/checkins_spec_helpers.rb
@@ -1,6 +1,6 @@
 module CheckinsSpecHelpers
 
-  def call_checkin_method(method, priv, delay, params, number, checkin)
+  def call_checkin_method(method, priv, delay, number, checkin)
     device.permission_for(developer).update! privilege: priv
     device.permission_for(developer).update! bypass_delay: delay
     get method.to_sym, params
@@ -8,11 +8,4 @@ module CheckinsSpecHelpers
     expect(res_hash.first['id']).to be(checkin.id) if checkin
   end
 
-  def call_index(priv, delay, params, number, checkin)
-    device.permission_for(developer).update! privilege: priv
-    device.permission_for(developer).update! bypass_delay: delay
-    get :index, params
-    expect(res_hash.size).to be number
-    expect(res_hash.first['id']).to be(checkin.id) if checkin
-  end
 end
