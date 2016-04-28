731b0cee1f1ccb83b7279f0dcba4bb92a4187275
Gary Siu <Gary@untypeable.com>
2016-04-28 16:57:24 +0100

    Fix n+1

--- app/controllers/api/v1/checkins_controller.rb
+++ app/controllers/api/v1/checkins_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::CheckinsController < Api::ApiController
     checkins = @user.get_checkins(@permissible, @device).order(created_at: :desc) \
       .paginate(page: params[:page], per_page: per_page)
     paginated_response_headers(checkins)
-    checkins = checkins.map do |checkin|
+    checkins = checkins.includes(:device).map do |checkin|
       checkin.resolve_address(@permissible, params[:type])
     end
     render json: hide_fogging_info(checkins)
