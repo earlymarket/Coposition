f5692df80e97bc83771ccb70f9d837cfa90b3f95
t-harps <t.w.harps@gmail.com>
2016-04-28 17:22:28 +0100

    tried to remove some more duplication

--- spec/controllers/api/v1/users/checkins_controller_privilege_spec.rb
+++ spec/controllers/api/v1/users/checkins_controller_privilege_spec.rb
@@ -26,80 +26,42 @@ RSpec.describe Api::V1::CheckinsController, type: :controller do
         checkin
     end
 
-    describe "GET #last" do
+    describe "GET #last/#index" do
 
       context "with privilege set to disallowed and bypass_delay set to false" do
         it "should return 0 checkins" do
-          call_checkin_method('last', 0, false, 0, nil)
+          ['last', 'index'].each { |method| call_checkin_method(method, 0, false, 0, nil) }
         end
       end
 
       context "with privilege set to disallowed and bypass_delay set to true" do
         it "should return 0 checkins" do
-          call_checkin_method('last', 0, true, 0, nil)
+          ['last', 'index'].each { |method| call_checkin_method(method, 0, true, 0, nil) }
         end
       end
 
       context "with privilege set to last_only and bypass_delay set to true" do
         it "should return 1 new checkin" do
-          call_checkin_method('last', 1, true, 1, checkin)
+          ['last', 'index'].each { |method| call_checkin_method(method, 1, true, 1, checkin) }
         end
       end
 
       context "with privilege set to last_only and bypass_delay set to false" do
         it "should return 1 old checkin" do
-          call_checkin_method('last', 1, false, 1, historic_checkin)
+          ['last', 'index'].each { |method| call_checkin_method(method, 1, false, 1, historic_checkin) }
         end
       end
 
       context "with privilege set to complete and bypass_delay set to true" do
-        it "should return 1 new checkin" do
+        it "should return 1 new checkin for last and 2 checkins for index" do
           call_checkin_method('last', 2, true, 1, checkin)
-        end
-      end
-
-      context "with privilege set to complete and bypass_delay set to false" do
-        it "should return 1 old checkin" do
-          call_checkin_method('last', 2, false, 1, historic_checkin)
-        end
-      end
-    end
-
-    describe "GET #index" do
-
-      context "with privilege set to disallowed and bypass_delay set to false" do
-        it "should return 0 checkins" do
-          call_checkin_method('index', 0, false, 0, nil)
-        end
-      end
-
-      context "with privilege set to disallowed and bypass_delay set to true" do
-        it "should return 0 checkins" do
-          call_checkin_method('index', 0, true, 0, nil)
-        end
-      end
-
-      context "with privilege set to last_only and bypass_delay set to true" do
-        it "should return 1 new checkin" do
-          call_checkin_method('index', 1, true, 1, checkin)
-        end
-      end
-
-      context "with privilege set to last_only and bypass_delay set to false" do
-        it "should return 1 old checkin" do
-          call_checkin_method('index', 1, false, 1, historic_checkin)
-        end
-      end
-
-      context "with privilege set to complete and bypass_delay set to true" do
-        it "should return 2 checkins" do
           call_checkin_method('index', 2, true, 2, checkin)
         end
       end
 
       context "with privilege set to complete and bypass_delay set to false" do
         it "should return 1 old checkin" do
-          call_checkin_method('index', 2, false, 1, historic_checkin)
+          ['last', 'index'].each { |method| call_checkin_method(method, 2, false, 1, historic_checkin) }
         end
       end
     end
@@ -116,7 +78,6 @@ RSpec.describe Api::V1::CheckinsController, type: :controller do
       context "with privilege set to disallowed and bypass_delay set to false" do
         it "should return 0 checkins" do
           call_checkin_method('last', 0, false, 0, nil)
-
         end
       end
 
