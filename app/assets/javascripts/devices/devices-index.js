a0f8fbe0ab6f13ae36531721639dd332cd8df116
t-harps <t.w.harps@gmail.com>
2016-05-06 09:48:30 +0100

    made sure to wait for google.charts to be loaded before initializing calender

--- app/assets/javascripts/devices/devices-index.js
+++ app/assets/javascripts/devices/devices-index.js
@@ -3,7 +3,7 @@ $(document).on('page:change', function() {
     COPO.utility.gonFix();
     COPO.permissions.initSwitches('devices', gon.current_user_id, gon.permissions)
     COPO.slider.initSliders(gon.devices);
-    COPO.calendar.refreshCalendar(gon.checkins);
+    google.charts.setOnLoadCallback(function(){ COPO.calendar.refreshCalendar(gon.checkins) });
     window.initPage = function(){
       $('.clip_button').off();
       COPO.utility.initClipboard();
