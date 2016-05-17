3873ac280adfc3280fa66fbf23a1306cfc9c8044
t-harps <t.w.harps@gmail.com>
2016-05-17 17:17:22 +0100

    spacing

--- spec/controllers/api/v1/developers_controller_spec.rb
+++ spec/controllers/api/v1/developers_controller_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe Api::V1::DevelopersController, type: :controller do
   let(:dev) { FactoryGirl::create :developer }
 
   before do
-      request.headers['X-Api-Key'] = dev.api_key
-    end
+    request.headers['X-Api-Key'] = dev.api_key
+  end
+
   describe '#index' do
     it 'should return a list of developers (id, company name, email)' do
       get :index, format: :json
@@ -17,10 +18,7 @@ RSpec.describe Api::V1::DevelopersController, type: :controller do
 
   describe '#show' do
     it 'should return a developers public info' do
-      get :show, {
-        id: dev.id,
-        format: :json
-      }
+      get :show, { id: dev.id, format: :json }
       expect(res_hash.keys).to eq dev.public_info.attributes.keys.map(&:to_sym)
     end
   end
