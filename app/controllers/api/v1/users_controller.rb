f3d215f33ce76e47f84d878e9abaa5f43703d468
Gary Siu <Gary@untypeable.com>
2016-05-19 17:35:25 +0100

    Fix filter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::UsersController < Api::ApiController
   respond_to :json
 
-  skip_before_filter :find_user, only: :index
-  before_action :check_user_approved_approvable, only: :show
+  before_action :check_user_approved_approvable
 
   def show
     @user = @user.public_info unless req_from_coposition_app?
