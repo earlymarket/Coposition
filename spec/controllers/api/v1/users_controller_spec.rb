051573b6a63c52a84ce0902bec9a6ddef7ad95bb
Gary Siu <Gary@untypeable.com>
2016-05-19 17:50:55 +0100

    Fix spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   include ControllerMacros
 
-  let(:device) { FactoryGirl::create :device }
   let(:dev) { FactoryGirl::create :developer }
-  let(:user) do
-    us = FactoryGirl::create :user
-    Approval.link(us,dev,'Developer')
-    Approval.accept(us,dev,'Developer')
-    us.devices << device
-    us
-  end
+  let(:user) { FactoryGirl::create :user }
 
   context 'with a correct API key' do
     before do
       request.headers['X-Api-Key'] = dev.api_key
+    end
+
+    it 'should reject an unapproved user' do
+      get :show, {
+        id: user.id,
+        format: :json
+      }
+      expect(response.status).to eq 401
     end
 
     it 'should assign User.id(:id) to @user if the developer is approved' do
