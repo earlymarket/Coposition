7a59541b4905f3959f64d3de4f6737eca704621b
t-harps <t.w.harps@gmail.com>
2016-05-20 10:03:46 +0100

    added a user authentication route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::UsersController < Api::ApiController
   respond_to :json
 
-  skip_before_filter [:find_user, :authenticate], only: :auth
-  before_action :check_user_approved_approvable
+  skip_before_filter :find_user,  only: :auth
+  skip_before_filter :authenticate, only: :auth
+  before_action :check_user_approved_approvable, only: :show
 
   def show
     @user = @user.public_info unless req_from_coposition_app?
@@ -10,11 +11,11 @@ class Api::V1::UsersController < Api::ApiController
   end
 
   def auth
-    user = User.find_by(email: params[:email], password: params[:password])
-    if user
-      render status: 204
+    user = User.find_by(email: params[:email])
+    if user && user.valid_password?(params[:password])
+      render status: 204, json: {message: 'success'}
     else
-      render status: 400
+      render status: 400, json: {message: 'email or password does not match'}
     end
   end
 end
