b933f8df300b085084524001cd7d349bb3c8aee6
t-harps <t.w.harps@gmail.com>
2016-05-20 12:10:34 +0100

    dont think i need the ssl stuff here and what i was doing was bad apparently, could just use httparty...

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,8 +6,6 @@ class Subscription < ActiveRecord::Base
   def send_data(data)
     uri = URI.parse("https://zapier.com/")
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new(target_url)
     request.add_field('Content-Type', 'application/json')
     request.body = data
