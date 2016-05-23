7d5493c7758ae1c8a9debe016f92fccb09df2e29
t-harps <t.w.harps@gmail.com>
2016-05-23 13:29:48 +0100

    test all the slack messages

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Request, type: :model do
     it "should have a developer" do
       expect(request.developer).to eq developer
     end
-  end 
+  end
 
+  describe "slack" do
+    it "should generate a helpful message for slack" do
+      expect(request.slack_message).to eq "A developer has made a new request, developer id: #{request.developer_id}, controller: #{request.controller}, action: #{request.action}, user_id: #{request.user_id}."
+    end
+  end
 end
