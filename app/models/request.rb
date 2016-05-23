30bd584b57a28cadf1cbcb9150905a48db921228
t-harps <t.w.harps@gmail.com>
2016-05-23 13:05:32 +0100

    added slack message for requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,12 @@
 class Request < ActiveRecord::Base
+  include SlackNotifiable
   belongs_to :developer
   scope :recent, ->(time) { where("created_at > ?", time) }
   default_scope { order(created_at: :desc) }
+
+  def slack_message
+    unless (controller == "api/v1/checkins" && action == "create") || (controller == "api/v1/users/requests")
+      "A developer has made a new request, developer id: #{self.developer_id}, controller: #{self.controller}, action: #{self.action}, user_id: #{self.user_id}."
+    end
+  end
 end
