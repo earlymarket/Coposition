feb9ea60fb1b177f7b702139e924017ac6fe0949
t-harps <t.w.harps@gmail.com>
2016-05-26 13:51:19 +0100

    updated style of helper specs

--- spec/helpers/devices_helper_spec.rb
+++ spec/helpers/devices_helper_spec.rb
@@ -1,11 +1,11 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe DevicesHelper, :type => :helper do
+RSpec.describe DevicesHelper, type: :helper do
   let(:safebuffer) { ActiveSupport::SafeBuffer }
-  let(:developer) { FactoryGirl::create :developer }
-  let(:user) { FactoryGirl::create :user }
+  let(:developer) { FactoryGirl.create :developer }
+  let(:user) { FactoryGirl.create :user }
   let(:device) do
-    device = FactoryGirl::create(:device, user_id: 1)
+    device = FactoryGirl.create(:device, user_id: 1)
     device.developers << developer
     device.permitted_users << user
     device
@@ -13,7 +13,7 @@ RSpec.describe DevicesHelper, :type => :helper do
   let(:other) { Device.update(device.id, published: true) }
 
   describe '#devices_permitted_actors_for' do
-    it "returns the devices developers and permitted users" do
+    it 'returns the devices developers and permitted users' do
       expect(helper.devices_permitted_actors_for(device)).to include(developer && user)
     end
   end
@@ -23,45 +23,42 @@ RSpec.describe DevicesHelper, :type => :helper do
       expect(helper.devices_last_checkin(device)).to match('No Checkins found')
     end
 
-    it "returns the last checkin address if it exists" do
-      checkin = FactoryGirl::create(:checkin, {
-        device_id: device.id,
-      })
+    it 'returns the last checkin address if it exists' do
+      checkin = FactoryGirl.create(:checkin, device_id: device.id)
       expect(helper.devices_last_checkin(device)).to include(checkin.city)
     end
   end
 
-  describe "#devices_delay_icon" do
-    it "returns different icons depending on a boolean input" do
+  describe '#devices_delay_icon' do
+    it 'returns different icons depending on a boolean input' do
       expect(helper.devices_delay_icon(true)).not_to eq(helper.devices_delay_icon(false))
       expect(helper.devices_delay_icon(true)).to match('icon')
       expect(helper.devices_delay_icon(false)).to match('icon')
     end
   end
 
-  describe "#devices_shared_icon" do
-    it "returns different icons on a devices published state" do
+  describe '#devices_shared_icon' do
+    it 'returns different icons on a devices published state' do
       expect(helper.devices_shared_icon(device)).not_to eq(helper.devices_shared_icon(other))
       expect(helper.devices_shared_icon(device)).to match('icon')
       expect(helper.devices_shared_icon(other)).to match('icon')
     end
   end
 
-  describe "#devices_access_icon" do
-    it "returns an icon" do
+  describe '#devices_access_icon' do
+    it 'returns an icon' do
       expect(helper.devices_access_icon).to match('icon')
     end
   end
 
-  describe "#devices_shared_link" do
-    it "return nothing if not published" do
+  describe '#devices_shared_link' do
+    it 'return nothing if not published' do
       expect(helper.devices_shared_link(device)).to be(nil)
     end
-    it "return the path to the shared device if device is published" do
+    it 'return the path to the shared device if device is published' do
       expect(helper.devices_shared_link(other)).to be_kind_of(String)
       # http://test.host/users/1/devices/5/shared
-      expect(helper.devices_shared_link(other)).to match(/http:\/\/.+\/users\/#{device.user_id}\/devices\/#{device.id}\/shared*+/)
+      expect(helper.devices_shared_link(other)).to match(%r{http://.+/users/#{device.user_id}/devices/#{device.id}/shared*+})
     end
   end
-
 end
--- spec/helpers/permissions_helper_spec.rb
+++ spec/helpers/permissions_helper_spec.rb
@@ -1,14 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe DevicesHelper, :type => :helper do
+RSpec.describe DevicesHelper, type: :helper do
   let(:safebuffer) { ActiveSupport::SafeBuffer }
-  let(:user) { FactoryGirl::create(:user) }
-  let(:developer) { FactoryGirl::create(:developer) }
-  let(:permission) {
-    device = FactoryGirl::create(:device, user_id: user.id)
+  let(:user) { FactoryGirl.create(:user) }
+  let(:developer) { FactoryGirl.create(:developer) }
+  let(:permission) do
+    device = FactoryGirl.create(:device, user_id: user.id)
     device.developers << developer
     Permission.last
-  }
+  end
 
   describe '#permissible_title' do
     it 'should accept either a user or a developer' do
@@ -17,46 +17,46 @@ RSpec.describe DevicesHelper, :type => :helper do
     end
 
     it "should return html with the user's email if it's a user" do
-      expect( helper.permissible_title(user) ).to match(user.email)
-      expect( helper.permissible_title(user).class ).to eq(safebuffer)
+      expect(helper.permissible_title(user)).to match(user.email)
+      expect(helper.permissible_title(user).class).to eq(safebuffer)
     end
 
     it "should return html with the company name if it's a developer" do
-      expect( helper.permissible_title(developer).class ).to eq(safebuffer)
-      expect( helper.permissible_title(developer) ).to match(ERB::Util::h(developer.company_name))
+      expect(helper.permissible_title(developer).class).to eq(safebuffer)
+      expect(helper.permissible_title(developer)).to match(ERB::Util.h(developer.company_name))
     end
   end
 
   describe '#permissions_control_class and #permissions_for_all' do
     it 'should return a string if permissionable is not a Permission' do
-      ([['permissions_control_class', 'master'], ['permissions_for_all', 'for all']]).each do |method, output|
-        expect( (helper.send(method, developer)).class ).to eq(String)
-        expect( helper.send(method, developer) ).to include(output)
+      [%w(permissions_control_class master), %w(permissions_for_all for all)].each do |method, output|
+        expect(helper.send(method, developer).class).to eq(String)
+        expect(helper.send(method, developer)).to include(output)
       end
     end
   end
 
   describe '#permissions_label_id' do
     it 'should return the permissionable id and switch type if a Permission' do
-      expect( helper.permissions_label_id(permission, 'disallowed') ).to include(permission.id.to_s)
-      expect( helper.permissions_label_id(developer, 'disallowed') ).to be(nil)
+      expect(helper.permissions_label_id(permission, 'disallowed')).to include(permission.id.to_s)
+      expect(helper.permissions_label_id(developer, 'disallowed')).to be(nil)
     end
   end
 
   describe '#permissions_switch_class' do
     it 'should return a different string depending on whether permissible is a Permission or not' do
-      expect( helper.permissions_switch_class(permission) ).to include('permission')
-      expect( helper.permissions_switch_class(developer) ).to include('master')
+      expect(helper.permissions_switch_class(permission)).to include('permission')
+      expect(helper.permissions_switch_class(developer)).to include('master')
     end
   end
 
   describe '#permissions_check_box_value' do
     it 'should return a boolean value depending on if permissionable is a Permission depending on the type' do
       permission.update(privilege: 'disallowed', bypass_delay: false, bypass_fogging: true)
-      expect( helper.permissions_check_box_value(permission, 'disallowed')).to eq true
-      expect( helper.permissions_check_box_value(permission, 'last_only')).to eq false
-      expect( helper.permissions_check_box_value(permission, 'bypass_delay')).to eq false
-      expect( helper.permissions_check_box_value(permission, 'bypass_fogging')).to eq true
+      expect(helper.permissions_check_box_value(permission, 'disallowed')).to eq true
+      expect(helper.permissions_check_box_value(permission, 'last_only')).to eq false
+      expect(helper.permissions_check_box_value(permission, 'bypass_delay')).to eq false
+      expect(helper.permissions_check_box_value(permission, 'bypass_fogging')).to eq true
     end
   end
 end
