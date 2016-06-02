9d5787c7d4f5bfcb2d9a758a2e17a13fde9c16c7
t-harps <t.w.harps@gmail.com>
2016-06-02 12:35:06 +0100

    added test for developer with subscription

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,8 @@ class UserMailer < ApplicationMailer
   end
 
   def add_user_email(from, added_user, developer)
-    base_url = "https://coposition.com/users/#{added_user.id}/"
-    @url = developer ? base_url + 'apps' : base_url + 'friends'
+    @url = "https://coposition.com/users/#{added_user.id}/"
+    @url += developer ? 'apps' : 'friends'
     @from = developer ? from.company_name : from.email
     mail(to: added_user.email, subject: "Coposition approval request from #{@from}")
   end
