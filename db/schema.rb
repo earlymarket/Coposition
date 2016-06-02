c95b1bd16fd5d88a4dc8b461051228c04b536906
t-harps <t.w.harps@gmail.com>
2016-06-02 16:22:26 +0100

    ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520152103) do
+ActiveRecord::Schema.define(version: 20160602144328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,14 @@ ActiveRecord::Schema.define(version: 20160520152103) do
   end
 
   add_index "cities", ["latitude", "longitude"], name: "index_cities_on_latitude_and_longitude", using: :btree
+
+  create_table "configs", force: :cascade do |t|
+    t.integer  "developer_id"
+    t.integer  "device_id"
+    t.text     "custom"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "developers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -139,11 +147,12 @@ ActiveRecord::Schema.define(version: 20160520152103) do
   end
 
   create_table "subscriptions", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "subscriber_id"
     t.string   "target_url"
     t.string   "event"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "subscriber_type"
   end
 
   create_table "users", force: :cascade do |t|
