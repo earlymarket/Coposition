779091f0b751416071deecb07c13f75ee2fa7651
t-harps <t.w.harps@gmail.com>
2016-06-07 17:03:02 +0100

    fixed bug where smooch would not be rendered on the dashboard

--- app/assets/javascripts/smooch.js
+++ app/assets/javascripts/smooch.js
@@ -1,11 +1,15 @@
 window.COPO = window.COPO || {};
 window.COPO.smooch = {
   initSmooch: function(user){
-    Smooch.init({
-      appToken: "48zalrms2pp1raaolssv7dry8",
-      userId: user.id.toString(),
-      email: user.email,
-      givenName: user.username
-    });
+    if(Smooch.appToken){
+      Smooch.render()
+    } else {
+      Smooch.init({
+        appToken: "48zalrms2pp1raaolssv7dry8",
+        userId: user.id.toString(),
+        email: user.email,
+        givenName: user.username
+      });
+    }
   }
 }
