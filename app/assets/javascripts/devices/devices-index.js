e378929465201d9dad8730a617a1448c10ffb851
Gary Siu <Gary@untypeable.com>
2016-06-08 11:26:45 +0100

    Added frontend changes to allow inline device name edits

--- app/assets/javascripts/devices/devices-index.js
+++ app/assets/javascripts/devices/devices-index.js
@@ -1,12 +1,49 @@
 $(document).on('page:change', function() {
   if ($(".c-devices.a-index").length === 1) {
-    COPO.utility.gonFix();
+    var U = COPO.utility;
+    U.gonFix();
     COPO.permissions.initSwitches('devices', gon.current_user_id, gon.permissions)
     COPO.delaySlider.initSliders(gon.devices);
     google.charts.setOnLoadCallback(function(){ COPO.calendar.refreshCalendar(gon.checkins) });
+
+    $('body').on('click', '.edit-button', function (e) {
+      e.preventDefault();
+      $(this).toggleClass('hide', true);
+      makeEditable($(this).prev(), handleEdited);
+    });
+
+    function makeEditable($target, handler) {
+      var original = $target.text();
+      $target.attr('contenteditable', true);
+      $target.focus();
+      document.execCommand('selectAll', false, null);
+      $target.on('blur', function () {
+        handler(original, $target);
+      });
+      $target.on('keydown', function (e) {
+        if(e.which === 27 || e.which === 13 ) {
+          handler(original, $target);
+        }
+      });
+      $target.on('click', function (e) {
+        e.preventDefault();
+      });
+      return $target;
+    };
+
+    function handleEdited(original, $target) {
+      if(original !== $target.text()) {
+        console.log($target.text());
+      };
+      $target.attr('contenteditable', false);
+      $target.next().toggleClass('hide', false);
+      U.deselect();
+      $target.off();
+    };
+
     window.initPage = function(){
       $('.clip_button').off();
-      COPO.utility.initClipboard();
+      U.initClipboard();
       $('.tooltipped').tooltip('remove');
       $('.tooltipped').tooltip({delay: 50});
       $('.linkbox').off('touchstart click');
@@ -34,6 +71,7 @@ $(document).on('page:change', function() {
     $(document).on('page:before-unload', function(){
       COPO.permissions.switchesOff();
       $(window).off("resize");
+      $('body').off('click', '.edit-button');
     })
   }
 })
