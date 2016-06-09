e76dd3a43ca3ef473b0bf6b4bcb5de93d1280b87
Gary Siu <Gary@untypeable.com>
2016-06-09 17:02:31 +0100

    better default options?

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -9,11 +9,11 @@ BowerRails.configure do |bower_rails|
   bower_rails.resolve_before_precompile = true
 
   # Invokes rake bower:clean before precompilation. Defaults to false
-  bower_rails.clean_before_precompile = true
+  # bower_rails.clean_before_precompile = true
 
   # Invokes rake bower:install:deployment instead rake bower:install. Defaults to false
   # bower_rails.use_bower_install_deployment = true
   #
   # Invokes rake bower:install and rake bower:install:deployment with -F (force) flag. Defaults to false
-  # bower_rails.force_install = true
+  bower_rails.force_install = true
 end
