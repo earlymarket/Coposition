263c917c8250a3f846a3dbefa56fc20b89f7c169
Gary Siu <Gary@Garys-MacBook-Air.local>
2016-06-15 14:38:42 +0100

    Rearrange the if block to make sure what I think is happening is happening

--- features/step_definitions/map_steps.rb
+++ features/step_definitions/map_steps.rb
@@ -4,7 +4,10 @@ Given(/^I right click on the map when it's ready$/) do
     find(:id, 'map').right_click
     expect(page).to have_content('Create checkin here')
   rescue RSpec::Expectations::ExpectationNotMetError
-    retry if Time.now - start_time < Capybara.default_max_wait_time
-    raise RSpec::Expectations::ExpectationNotMetError
+    if Time.now - start_time < Capybara.default_max_wait_time
+      retry
+    else
+      raise RSpec::Expectations::ExpectationNotMetError
+    end
   end
 end
