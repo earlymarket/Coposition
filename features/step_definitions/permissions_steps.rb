9111878d841012660b3dca72567ee839ec74af6e
Gary Siu <Gary@Garys-MacBook-Air.local>
2016-06-15 16:58:11 +0100

    Removed old blob of tests

--- features/step_definitions/permissions_steps.rb
+++ features/step_definitions/permissions_steps.rb
@@ -24,21 +24,3 @@ end
 Given(/^I should have privilege set to "(.*?)"$/) do |value|
   wait_until { Permission.last.privilege == value }
 end
-
-Given(/^I change my permissions$/) do
-  steps %(
-    Given I click the link "lock"
-      And I click the switch "bypass-fogging"
-      And I click the switch "bypass-delay"
-      And I click the switch "last-only"
-        Then I should have "bypass_fogging" enabled
-        And I should have "bypass_delay" enabled
-        And I should have privilege set to "last_only"
-      When I click the switch "disallowed"
-        Then I should have privilege set to "disallowed"
-      And I click the switch "last-only"
-      And I click the switch "bypass-fogging"
-        Then I should have privilege set to "disallowed"
-        And I should have "bypass_delay" enabled
-  )
-end
