34fa7e775a2119c80ec8541f4824a180c0f092aa
t-harps <t.w.harps@gmail.com>
2016-06-20 10:53:53 +0100

    deleted all the dropdown stuff

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -2,8 +2,4 @@ module NavbarHelper
   def navbar_indicator(text, count)
     count > 0 ? text.html_safe + ' <div class="indicator"></div>'.html_safe : text
   end
-
-  def navbar_dropdown(text, count)
-    count > 0 ? text.html_safe + '<i class="dropdown-arrow material-icons right">arrow_drop_down</i>'.html_safe : text
-  end
 end
