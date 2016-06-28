1c628222b74bd6bdc8b21d29ecd6242c3edcdb13
Gary Siu <Gary@untypeable.com>
2016-06-28 11:49:59 +0100

    Typo fix

--- public/assets/application-6e1781c6886cf33b3dfaf268b5e4c1a7b3e3ed3f6ab537292bf46ac1da38a237.js
+++ public/assets/application-6e1781c6886cf33b3dfaf268b5e4c1a7b3e3ed3f6ab537292bf46ac1da38a237.js
@@ -47079,7 +47079,7 @@ $(document).on('page:change', function () {
             return L.latLng(checkin.lat, checkin.lng);
           }));
         },
-        status: 'Some of your last month\'s check-ins (random sample). <a href=\'./devices\'>(Fore details)</a>',
+        status: 'Some of your last month\'s check-ins (random sample). <a href=\'./devices\'>(More details)</a>',
         init: function init(caller) {
           if (this.hasCheckins()) {
             caller.slides.push({
--- public/assets/user-dashboard-5850f1e9d104a9a28729893882cb4cda979f9120317d48357181a5af933c6664.es6
+++ public/assets/user-dashboard-5850f1e9d104a9a28729893882cb4cda979f9120317d48357181a5af933c6664.es6
@@ -123,7 +123,7 @@ $(document).on('page:change', function () {
           gon.months_checkins.map(checkin => L.latLng(checkin.lat, checkin.lng))
         );
       },
-      status: `Some of your last month's check-ins (random sample). <a href='./devices'>(Fore details)</a>`,
+      status: `Some of your last month's check-ins (random sample). <a href='./devices'>(More details)</a>`,
       init (caller) {
         if (this.hasCheckins()) {
           caller.slides.push({
