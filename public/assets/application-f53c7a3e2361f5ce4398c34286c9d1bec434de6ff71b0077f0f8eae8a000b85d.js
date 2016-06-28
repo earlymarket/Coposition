bd8157a44f8920038ba588fd1a3916a603b27440
Gary Siu <Gary@untypeable.com>
2016-06-28 11:37:18 +0100

    Assets

--- public/assets/application-f53c7a3e2361f5ce4398c34286c9d1bec434de6ff71b0077f0f8eae8a000b85d.js
+++ public/assets/application-f53c7a3e2361f5ce4398c34286c9d1bec434de6ff71b0077f0f8eae8a000b85d.js
@@ -47079,7 +47079,7 @@ $(document).on('page:change', function () {
             return L.latLng(checkin.lat, checkin.lng);
           }));
         },
-        status: 'Your last month\'s check-ins <a href=\'./devices\'>(more details)</a>',
+        status: 'Some of your last month\'s check-ins (random sample). <a href=\'./devices\'>(Fore details)</a>',
         init: function init(caller) {
           if (this.hasCheckins()) {
             caller.slides.push({
