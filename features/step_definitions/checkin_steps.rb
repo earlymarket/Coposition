2ab9045cbfc3b4fead585091e1c0eb7d1b6eda44
t-harps <t.w.harps@gmail.com>
2016-08-09 11:47:03 +0100

    updated staging to match dev

--- features/step_definitions/checkin_steps.rb
+++ features/step_definitions/checkin_steps.rb
@@ -2,10 +2,6 @@ Then(/^I(?: should)? have (\d+) checkins on the map$/) do |number|
   wait_until { page.has_selector?('img.leaflet-marker-icon', count: number.to_i) }
 end
 
-Then(/^I(?: should)? have (\d+) checkins in the table$/) do |number|
-  page.has_selector?('tr', count: number.to_i + 1)
-end
-
 Given(/^I click on my last checkin$/) do
   find("img[alt='lastCheckin']").click
 end
