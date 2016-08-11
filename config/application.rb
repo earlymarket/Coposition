5d1a9312b2f4c53813c0a80732deb18ddfb59539
t-harps <t.w.harps@gmail.com>
2016-08-11 14:30:52 +0100

    updated will paginate to fix railties bug and ran rails:db migrate

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module Coposition
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
