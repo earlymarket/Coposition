44249ac408e7526797b9c9bdfd0676914d088783
Gary Siu <Gary@untypeable.com>
2016-08-22 12:38:08 +0100

    Restore accidentally deleted friends helper

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -8,5 +8,10 @@ module FriendsHelper
   end
 
   def friends_last_checkin(checkins)
+    if checkins.present?
+      "<p>Last available reported area: #{checkins.first.fogged_area}</p>".html_safe
+    else
+      '<p>No location found</p>'.html_safe
+    end
   end
 end
