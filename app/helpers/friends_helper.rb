2f1ffc466f9e8dac5586f5f909dd5aa76bf7f563
t-harps <t.w.harps@gmail.com>
2016-08-26 15:27:20 +0100

    deleted unused friends_last_checkin method and fixed tests

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -6,12 +6,4 @@ module FriendsHelper
       '<p>No location found</p>'.html_safe
     end
   end
-
-  def friends_last_checkin(checkins)
-    if checkins.present?
-      "<p>Last available reported area: #{checkins.first.fogged_area}</p>".html_safe
-    else
-      '<p>No location found</p>'.html_safe
-    end
-  end
 end
