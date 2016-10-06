16d0606d2ffb74744b2019f53aeba6074c24c83f
t-harps <t.w.harps@gmail.com>
2016-10-06 11:06:59 +0100

    removed slack notifiable from requests so message not created for every developer request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,4 @@
 class Request < ApplicationRecord
-  include SlackNotifiable
   belongs_to :developer
   scope :recent, ->(time) { where('created_at > ?', time) }
   default_scope { order(created_at: :desc) }
