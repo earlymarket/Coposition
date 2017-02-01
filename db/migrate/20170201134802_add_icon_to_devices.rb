70cf8e96f1ef79a8f96a0abbe4a0622f726f8d23
t-harps <t.w.harps@gmail.com>
2017-02-01 13:55:40 +0000

    remembered to actually update migration

--- db/migrate/20170201134802_add_icon_to_devices.rb
+++ db/migrate/20170201134802_add_icon_to_devices.rb
@@ -1,5 +1,5 @@
 class AddIconToDevices < ActiveRecord::Migration[5.0]
   def change
-  	add_column :checkins, :fogged_country_code, :string, default: 'devices_other'
+  	add_column :devices, :icon, :string, default: 'devices_other'
   end
 end
