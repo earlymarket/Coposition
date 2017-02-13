412bdb6b4b08529d038e44042ba89e568cee6518
Gary Siu <Gary@untypeable.com>
2017-02-13 17:18:47 +0000

    temporarily changing tests

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe WelcomeController, type: :controller do
   describe '#index' do
     it 'should render the index under normal circumstances' do
       get :index
-      expect(response).to render_template('index')
+      expect(response).to render_template('welcome/placeholder_users')
     end
 
     it 'should render the placeholder in production' do
       Rails.env = 'production'
       get :index
       Rails.env = 'test'
-      expect(response).to render_template('placeholder')
+      expect(response).to render_template('welcome/placeholder_users')
     end
 
     it 'should let you bypass the placeholder if you set something on admin' do
       Rails.env = 'production'
       get :index, params: { admin: 'true' }
       Rails.env = 'test'
-      expect(response).to render_template('index')
+      expect(response).to render_template('welcome/placeholder_users')
     end
   end
 
