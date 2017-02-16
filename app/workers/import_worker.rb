76a0f9458e3625473f9b423d11563e570a878d3e
t-harps <t.w.harps@gmail.com>
2017-02-16 14:28:16 +0000

    added spacing and split methods up in importworker

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -2,14 +2,26 @@ class ImportWorker
   include Sidekiq::Worker
 
   def perform(device, json_file)
-    file_array = JSON.parse(json_file)
     Checkin.transaction do
-      file_array.drop(1).each do |row|
-        attr = row.split(',')
-        checkin = Checkin.find_by_id(attr[0]) || Checkin.new
-        checkin.assign_attributes(lat: attr[1], lng: attr[2], created_at: attr[3], fogged: attr[11], device_id: device)
-        checkin.save!
+      file_array(json_file).drop(1).each do |row|
+        checkin_create_or_update_from_row!(row, device)
       end
     end
+  end
+
+  def file_array(json_file)
+    JSON.parse(json_file)
+  end
+
+  def checkin_create_or_update_from_row!(row, device)
+    attrs = attributes_from_row(row, device)
+    checkin = Checkin.find_by_id(attrs.delete(:id)) || Checkin.new
+    checkin.assign_attributes(attrs)
+    checkin.save!
+  end
+
+  def attributes_from_row(row, device)
+    attr = row.split(",")
+    { id: attr[0], lat: attr[1], lng: attr[2], created_at: attr[3], fogged: attr[11], device_id: device }
   end
 end
