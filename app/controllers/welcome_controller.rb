56c7f16c37e4c30df18990fa6207847418d7762b
Gary Siu <Gary@untypeable.com>
2017-02-16 12:48:37 +0000

    fixed grouping of conditions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    # render 'placeholder', layout: false if Rails.env == 'production' && !params[:admin] && !signed_in?
-    render 'placeholder_users', layout: false if Rails.env == 'production' && !params[:admin] || !signed_in?
+    render 'placeholder_users', layout: false if Rails.env == 'production' && (!params[:admin] || !signed_in?)
+    # render 'placeholder_users', layout: false
   end
 
   def devs
-    render 'placeholder_devs', layout: false if Rails.env == 'production' && !params[:admin] || !signed_in?
+    render 'placeholder_devs', layout: false if Rails.env == 'production' && (!params[:admin] || !signed_in?)
+    # render 'placeholder_devs', layout: false
   end
 
   def api
