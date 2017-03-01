04eec3d2e5ff6536defce1c833eed0558dc39383
Dmitry Trager <dmitry.trager@flatstack.com>
2017-03-01 18:37:25 +0300

    [#4985] Add settings for staging/prod env of actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,13 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  # Action Cable endpoint configuration
+  config.action_cable.url = "wss://coposition.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [
+    "http://coposition.herokuapp.com",
+    "https://coposition.herokuapp.com"
+  ]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
