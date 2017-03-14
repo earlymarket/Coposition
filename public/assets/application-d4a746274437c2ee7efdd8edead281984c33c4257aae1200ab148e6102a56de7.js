db41e685b5cea78d24d79750027c50dd041b00ba
t-harps <t.w.harps@gmail.com>
2017-03-14 17:16:19 +0000

    assets

--- public/assets/application-d4a746274437c2ee7efdd8edead281984c33c4257aae1200ab148e6102a56de7.js
+++ public/assets/application-d4a746274437c2ee7efdd8edead281984c33c4257aae1200ab148e6102a56de7.js
@@ -55798,6 +55798,20 @@ App.friend = App.cable.subscriptions.create("FriendChannel", {
         COPO.maps.refreshMarkers(gon.checkins);
 
         break;
+
+      case "destroy":
+        if ($(".c-friends.a-show_device").length === 0) {
+          return;
+        };
+
+        var index = gon.checkins.findIndex(function (checkin) {
+          return checkin.id === data.msg.id;
+        });
+        gon.checkins.splice(index, 1);
+
+        COPO.maps.refreshMarkers(gon.checkins);
+
+        break;
     }
   }
 });
