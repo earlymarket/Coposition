c5345fd833b78394fc8f9ed1977d41d4bcd5d181
Gary Siu <Gary@untypeable.com>
2017-03-22 15:29:35 +0000

    trying without the dependency just to see what happens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,7 @@
 //= require typeahead.js/dist/typeahead.bundle.min
 //= require ion.rangeSlider/js/ion.rangeSlider.min
 //= require clipboard/dist/clipboard.min
-//= require slick-carousel/slick/slick
+
 // STILL more up to date version
 //= require nouislider.min
 
