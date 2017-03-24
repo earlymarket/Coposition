23bfa041faa5d9c6594f1a0223e8387dc2c3129d
t-harps <t.w.harps@gmail.com>
2017-03-24 16:06:20 +0000

    deleting puts log message

--- spec/support/cleanup_cloudinary.rb
+++ spec/support/cleanup_cloudinary.rb
@@ -1,7 +1,6 @@
 class CleanupCloudinary
   def self.clean
     Attachinary::File.all.each do |file|
-      puts 'deleting' + file.public_id
       Cloudinary::Uploader.destroy(file.public_id, resource_type: "raw")
     end
   end
