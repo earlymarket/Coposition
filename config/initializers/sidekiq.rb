26bcd7392c2c9f5baa9bdfff53307e7cb11b6fa4
t-harps <t.w.harps@gmail.com>
2017-03-24 14:28:39 +0000

    configuring number of connections

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ raw_config = template.result(binding)
 url = YAML.load(raw_config)[Rails.env]["url"]
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: url }
+  config.redis = { url: url, size: 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: url }
+  config.redis = { url: url, size: 1 }
 end
