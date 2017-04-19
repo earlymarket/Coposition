9a6809eeabe343bf06b6ab7340774d44405420f0
t-harps <t.w.harps@gmail.com>
2017-04-19 14:49:19 +0100

    added more styling and delete/edit/index(application) actions configured

--- app/controllers/release_notes_controller.rb
+++ app/controllers/release_notes_controller.rb
@@ -1,27 +1,38 @@
 class ReleaseNotesController < ApplicationController
+  before_action :find_note, only: %i(edit update destroy)
+
   def new
     @release_note = ReleaseNote.new
   end
 
   def create
     ReleaseNote.create(allowed_params)
-    redirect_to release_notes_path
+    redirect_to release_notes_path, notice: "Note created"
   end
 
   def index
-    @release_notes = ReleaseNote.all.paginate(per_page: 10, page: params[:page])
+    release_notes = ReleaseNote.all
+    release_notes = release_notes.where(application: params[:application]) if params[:application]
+    @release_notes = release_notes.paginate(per_page: 10, page: params[:page])
   end
 
-  def edit
-    @release_note = ReleaseNote.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @release_note = ReleaseNote.find(params[:id])
-    @release_note.update(params)
+    @release_note.update(allowed_params)
+    redirect_to release_notes_path, notice: "Note edited"
+  end
+
+  def destroy
+    @release_note.destroy
+    redirect_to release_notes_path, notice: "Note deleted"
   end
 
   private
+
+  def find_note
+    @release_note ||= ReleaseNote.find(params[:id])
+  end
 
   def allowed_params
     params.require(:release_note).permit(%i(version application content))
