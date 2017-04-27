5c9f90a0e8ca0e6653b9ff9a0134c8dfaa0c3815
t-harps <t.w.harps@gmail.com>
2017-04-27 15:11:55 +0100

    provided filter links to various activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,14 @@
 class ActivitiesController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").paginate(per_page: 15, page: params[:page])
+    activities = filter_params ? PublicActivity::Activity.where(filter_params) : PublicActivity::Activity.all
+    @activities = activities.order("created_at desc").paginate(per_page: 15, page: params[:page])
+  end
+
+  private
+
+  def filter_params
+    return false unless params[:filter]
+    params.require(:filter).permit(:trackable_type, :trackable_id, :owner_type, :owner_id, :key)
   end
 end
