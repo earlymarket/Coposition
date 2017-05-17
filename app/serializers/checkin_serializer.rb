6ec0a36f4d44fa18940a134b86fca00507cf1bdd
Dmitry Trager <dmitry.trager@flatstack.com>
2017-05-17 19:31:23 +0300

    Add other checkin attributes to serializer if they are fetched from DB

--- app/serializers/checkin_serializer.rb
+++ app/serializers/checkin_serializer.rb
@@ -1,4 +1,26 @@
 class CheckinSerializer < ActiveModel::Serializer
-  attributes :id, :device_id, :lat, :lng, :address, :city, :postal_code,
-    :created_at, :updated_at, :country_code
+  FILTERED_SET = %i(
+    id
+    device_id
+    lat
+    lng
+    address
+    city
+    postal_code
+    country_code
+    created_at
+    updated_at
+  )
+
+  attributes *FILTERED_SET
+
+  def attributes(*args)
+    h = super
+
+    (object.attribute_names.map(&:to_sym) - FILTERED_SET).each do |key|
+      h[key] = object.attributes[key]
+    end
+
+    h
+  end
 end
