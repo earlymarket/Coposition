73a9d89ab6c94a76349b24b76975178d4d93b142
Dmitry Trager <dmitry.trager@flatstack.com>
2017-05-17 18:58:35 +0300

    Add country_code to basic serialized attributes for checkin

--- app/serializers/checkin_serializer.rb
+++ app/serializers/checkin_serializer.rb
@@ -1,4 +1,4 @@
 class CheckinSerializer < ActiveModel::Serializer
   attributes :id, :device_id, :lat, :lng, :address, :city, :postal_code,
-    :created_at, :updated_at
+    :created_at, :updated_at, :country_code
 end
