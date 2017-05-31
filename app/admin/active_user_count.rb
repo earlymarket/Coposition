105f31e41ef02faf5dd938ed55f92872cc015dbb
Dmitry Trager <dmitry.trager@flatstack.com>
2017-05-31 17:56:45 +0300

    Add csv import for custom page reports

--- app/admin/active_user_count.rb
+++ app/admin/active_user_count.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register_page "Active User Count" do
+  HEADERS = %w[Month Total Growth]
+
   content do
     render "index", layout: "active_admin"
   end
@@ -7,5 +9,25 @@ ActiveAdmin.register_page "Active User Count" do
     def index
       params[:collection] = ItemsByMonthsQuery.new(table: nil, active_users: true).all
     end
+  end
+
+  page_action :csv, method: :get do
+    collection = ItemsByMonthsQuery.new(table: nil, active_users: true).all
+
+    csv = CSV.generate(encoding: "UTF-8") do |csv|
+      # add headers
+      csv << HEADERS
+      collection.each do |item|
+        csv << [item["month"], item["total"], "%.2f %" % (item["growth"].to_i * 100)]
+      end
+    end
+    # send file to user
+    send_data csv.encode("UTF-8"),
+      type: "text/csv; charset=windows-1251; header=present",
+      disposition: "attachment; filename=active_users_#{DateTime.now.to_s}.csv"
+  end
+
+  action_item :csv do
+    link_to "Export to CSV", admin_active_user_count_csv_path, method: :get
   end
 end
--- app/admin/device_distribution.rb
+++ app/admin/device_distribution.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register_page "Device Distribution" do
   DEVICE_NUMBERS = [0, 1, 2, 3, 4, 5]
   DISTRIBUTION_TYPES = %i(full active).freeze
+  HEADERS = %w[Devices Consumers Percent]
 
   content do
     render "index", layout: "active_admin"
@@ -69,5 +70,45 @@ ActiveAdmin.register_page "Device Distribution" do
         DEV_DISTR
       )
     end
+  end
+
+  page_action :full_csv, method: :get do
+    collection = full_distribution
+
+    csv = CSV.generate(encoding: "UTF-8") do |csv|
+      # add headers
+      csv << HEADERS
+      collection.each do |item|
+        csv << [item[:devices], item[:consumers], "%.2f %" % item[:percent].to_i]
+      end
+    end
+    # send file to user
+    send_data csv.encode("UTF-8"),
+      type: "text/csv; charset=windows-1251; header=present",
+      disposition: "attachment; filename=full_device_distribution_#{DateTime.now.to_s}.csv"
+  end
+
+  page_action :active_csv, method: :get do
+    collection = active_distribution
+
+    csv = CSV.generate(encoding: "UTF-8") do |csv|
+      # add headers
+      csv << HEADERS
+      collection.each do |item|
+        csv << [item[:devices], item[:consumers], "%.2f %" % item[:percent].to_i]
+      end
+    end
+    # send file to user
+    send_data csv.encode("UTF-8"),
+      type: "text/csv; charset=windows-1251; header=present",
+      disposition: "attachment; filename=active_device_distribution_#{DateTime.now.to_s}.csv"
+  end
+
+  action_item :full_csv do
+    link_to "All to CSV", admin_device_distribution_full_csv_path, method: :get
+  end
+
+  action_item :active_csv do
+    link_to "Active to CSV", admin_device_distribution_active_csv_path, method: :get
   end
 end
