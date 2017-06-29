1eafcb2e9b01189c412bafc83aa2f9934f3c07ea
t-harps <t.w.harps@gmail.com>
2017-06-29 12:21:20 +0100

    removing warnings

--- test/performance/show_friends_test.rb
+++ test/performance/show_friends_test.rb
@@ -1,7 +1,7 @@
 require "benchmark_helper"
 
 class Users::ApprovalsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, raise: false
 end
 
 class ShowFriendsTest < ActionDispatch::PerformanceTest
