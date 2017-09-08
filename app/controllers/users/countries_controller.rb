f27a1e334551d41fd900a4b79b962be7d097e49b
dmitrytrager <dmitry.trager@flatstack.com>
2017-09-08 03:41:40 +0300

    Add last_visited and full_history queries, add links to countries page

--- app/controllers/users/countries_controller.rb
+++ app/controllers/users/countries_controller.rb
@@ -1,14 +1,19 @@
 class Users::CountriesController < ApplicationController
   before_action :authenticate_user!
-  helper_method :countries_presenter
+  helper_method :countries
 
   def index
   end
 
   private
 
-  def countries_presenter
-    @countries_presenter ||= ::Users::DashboardsPresenter.new(current_user)
+  def countries
+    @countries = CountriesVisitPeriodQuery.new(user: current_user)
+    @countries = if params[:last_visited]
+      @countries.last_visited
+    else
+      @countries.full_history
+    end
   end
 end
 
--- app/queries/countries_visit_period_query.rb
+++ app/queries/countries_visit_period_query.rb
@@ -6,20 +6,39 @@ class CountriesVisitPeriodQuery
     @options = options
   end
 
-  def all
-    query = <<-EOQ
+  def full_history
+    ActiveRecord::Base.connection.execute(full_history_query)
+  end
+
+  def last_visited
+    ActiveRecord::Base.connection.execute(last_visited_query)
+  end
+
+  private
+
+  def full_history_query
+    <<-EOQ
       SELECT
         ch.country_code,
-        min(ch.created_at) OVER w,
-        max(ch.created_at) OVER w
+        MIN(ch.created_at) OVER w as min_date,
+        MAX(ch.created_at) OVER w as max_date
       FROM checkins as ch
       INNER JOIN
         devices ON ch.device_id = devices.id
       WHERE
         devices.user_id = #{user.id}
-      WINDOW w AS (PARTITION BY ch.country_code);
+      WINDOW w AS (PARTITION BY ch.country_code)
     EOQ
+  end
 
-    ActiveRecord::Base.connection.execute(query)
+  def last_visited_query
+    <<-EOQ
+      SELECT
+        ch.country_code,
+        MAX(ch.max_date) as max_date,
+        MAX(ch.min_date) as min_date
+      FROM (#{full_history_query}) ch
+      GROUP BY ch.country_code
+    EOQ
   end
 end
