6bb6f8bdb6348eb4cef17ca73d6d869959364094
dmitrytrager <dmitry.trager@flatstack.com>
2017-09-13 20:53:15 +0300

    Update DB version in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825152601) do
+ActiveRecord::Schema.define(version: 20170913173043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
