1e39315c9cf8800a61f82b44ed235dde36b9814a
t-harps <t.w.harps@gmail.com>
2017-09-27 14:07:36 +1300

    removing commented out line

--- app/controllers/concerns/custom_token_response.rb
+++ app/controllers/concerns/custom_token_response.rb
@@ -4,7 +4,6 @@ module CustomTokenResponse
       "user" => User.find(token.resource_owner_id).public_info_hash
     }
 
-    # call original `#body` method and merge its result with the additional data hash
     super.merge(additional_data)
   end
 end
