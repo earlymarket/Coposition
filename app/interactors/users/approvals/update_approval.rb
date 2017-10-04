be8eee26cafac9f8af250cbb44e1f54ad3714cb2
t-harps <t.w.harps@gmail.com>
2017-10-05 12:55:27 +1300

    adding push notification id for sender

--- app/interactors/users/approvals/update_approval.rb
+++ app/interactors/users/approvals/update_approval.rb
@@ -8,6 +8,7 @@ module Users::Approvals
       context.fail! unless approval
       Approval.accept(current_user, approvable, approvable_type)
       create_activity
+      notify_request_sender
       context.approval = approval
       context.approvable_type = approvable_type
       context.approvable = approvable
@@ -17,6 +18,17 @@ module Users::Approvals
 
     def create_activity
       CreateActivity.call(entity: approval, action: :update, owner: current_user, params: params.to_h)
+    end
+
+    def notify_request_sender
+      Firebase::Push.call(
+        topic: approvable.id,
+        content_available: true,
+        notification: {
+          body: "#{current_user.email} has accepted your friend request",
+          title: "New Friend"
+        }
+      )
     end
 
     def approval
