499edce143aa1adac8c5ffa6c2d4df5a69463c44
t-harps <t.w.harps@gmail.com>
2017-10-31 14:26:10 +0000

    added ability to set application of release notes as ios or android, added api controller for getting release notes by application and version

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
   namespace :api, path: "", constraints: { subdomain: "api" }, defaults: { format: "json" } do
     scope module: :v1, constraints: Constraints::ApiConstraint.new(version: 1, default: true) do
       resources :subscriptions, only: [:create, :destroy]
+      resources :release_notes, only: [:index, :show]
       resources :configs, only: [:index, :show, :update]
       resource :uuid, only: [:show]
       resources :checkins, only: [:create] do
