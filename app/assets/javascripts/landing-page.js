d59d4bff6307d1640919e9ad88ef4d33898c0bb8
t-harps <t.w.harps@gmail.com>
2017-11-18 16:27:23 -0800

    fixing styling for small screens

--- app/assets/javascripts/landing-page.js
+++ app/assets/javascripts/landing-page.js
@@ -23,7 +23,7 @@ $(document).on('ready page:change', function() {
   } else {
     var resize = function(){
       if(window.innerWidth<660){
-        $("main").css('padding-top', '80px');
+        $("main").css('padding-top', '73px');
       } else {
         $("main").css('padding-top', '60px');
       }
