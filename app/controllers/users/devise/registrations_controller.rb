802bd0e97eeff576bc34464fc46c388a177c2a76
t-harps <t.w.harps@gmail.com>
2017-12-22 19:50:42 -0800

    adding recaptcha to user page

--- app/controllers/users/devise/registrations_controller.rb
+++ app/controllers/users/devise/registrations_controller.rb
@@ -3,6 +3,7 @@ class Users::Devise::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :check_captcha, only: [:create]
 
   # GET /resource/sign_up
   # def new
@@ -74,6 +75,14 @@ class Users::Devise::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(_resource)
     return session[:return_to] if session[:return_to]
     user_dashboard_path(current_user)
+  end
+
+  def check_captcha
+    unless verify_recaptcha
+      self.resource = resource_class.new sign_up_params
+      resource.validate # Look for any other validation errors besides Recaptcha
+      respond_with_navigational(resource) { render :new }
+    end
   end
 
   # The path used after sign up for inactive accounts.
