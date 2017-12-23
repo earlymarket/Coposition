c00ab3664bfbf2f1d2fd7c6489e9ea06a12edb0e
t-harps <t.w.harps@gmail.com>
2017-12-23 11:37:43 -0800

    adding to initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.01'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
 Rails.application.config.assets.configure do |env|
   env.context_class.class_eval do
