ed94c6e55dd732223126778a6bfeda2a59ecc776
t-harps <t.w.harps@gmail.com>
2018-01-02 11:24:52 -0800

    adding spec for new controller and other changes

--- app/controllers/users/email_requests_controller.rb
+++ app/controllers/users/email_requests_controller.rb
@@ -1,7 +1,4 @@
 class Users::EmailRequestsController < ApplicationController
-  before_action :authenticate_user!, except: :add
-  before_action :correct_url_user?, except: :add
-
   def destroy
     EmailRequest.find(params[:id]).destroy
     approvals_presenter_and_gon(approvable_type: "User")
