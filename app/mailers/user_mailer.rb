08c7f1fa14da97acc0c002e1b9edfad53caa9601
t-harps <t.w.harps@gmail.com>
2018-01-02 15:32:30 -0800

    fixing subscription usermailer logic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
   end
 
   def invite_sent_email(user, friend_email)
-    return unless user.subscription == "all"
+    return unless user.subscription != "none"
     SendSendgridEmail.call(
       to: user.email, subject: "Coposition friend request sent", id: "3bc81984-bec7-49af-8612-4107c028f5f5",
       substitutions: [
@@ -23,7 +23,7 @@ class UserMailer < ApplicationMailer
   end
 
   def add_user_email(user, friend, from_developer)
-    return unless friend.subscription == "all"
+    return unless friend.subscription != "none"
     SendSendgridEmail.call(
       to: friend.email, subject: "Coposition approval request", id: "64b3b8c9-12ae-49bc-9983-2ac3e507ac0d",
       substitutions: [
@@ -35,7 +35,7 @@ class UserMailer < ApplicationMailer
   end
 
   def pending_request_email(approvable, user)
-    return unless user.subscription == "all"
+    return unless user.subscription != "none"
     SendSendgridEmail.call(
       to: user.email, subject: "Coposition approval request", id: "57af0f8b-2aa9-4621-86ce-139d527a57b8",
       substitutions: [
@@ -47,7 +47,7 @@ class UserMailer < ApplicationMailer
   end
 
   def request_accepted(user, friend)
-    return unless user.subscription == "all"
+    return unless user.subscription != "none"
     SendSendgridEmail.call(
       to: user.email, subject: "Coposition new friend", id: "dafcb547-5aec-4671-88a3-776cd38948a4",
       substitutions: [
@@ -60,7 +60,7 @@ class UserMailer < ApplicationMailer
   end
 
   def no_activity_email(device)
-    return unless device.user.subscription != "none"
+    return unless device.user.subscription == "all"
     SendSendgridEmail.call(
       to: device.user.email, subject: "Coposition activity", id: "b4437ee3-651a-4252-921b-c2a8ace722ac",
       substitutions: [
