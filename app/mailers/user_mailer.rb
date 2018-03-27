fba452613feb6a392d5524919b16b8173c0dd1d8
t-harps <t.w.harps@gmail.com>
2018-03-27 16:17:06 -0700

    fixing device link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -77,7 +77,7 @@ class UserMailer < ApplicationMailer
     content += "<ul>"
     inactive.each do |dev|
       last = dev.checkins.first
-      content += "<li><a href='https://coposition.com/users/#{dev.user.id}/devs/#{dev.id}'>"
+      content += "<li><a href='https://coposition.com/users/#{dev.user.id}/devices/#{dev.id}?first_load=true'>"
       content += "#{dev.name}</a>"
       content += " - Auto check-in #{dev.config.custom && dev.config.custom['active'] ? 'on' : 'off'}"
       content += " - #{dev.config.custom && dev.config.custom['assigned'] ? 'Assigned' : 'Unassigned'}"
